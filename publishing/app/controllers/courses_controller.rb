a5eba9cc8acaa43b03edded8127cedf66f38f962
Steven Thonus <steven@ln2.nl>
2014-06-03 16:30:47 +0200

    removed courses

--- publishing/app/controllers/courses_controller.rb
+++ publishing/app/controllers/courses_controller.rb
@@ -5,47 +5,47 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
-  def show
-  end
-
-  def new
-    @course = Course.new
-  end
-
-  def edit
-  end
-
-  def create
-    @course = Course.new(course_params)
-    if @course.save
-      redirect_to courses_path, notice: 'Course was successfully created.'
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @course.update(course_params)
-      redirect_to @course, notice: 'Course was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @course.trash
-    redirect_to courses_url, notice: 'Course was successfully destroyed.'
-  end
-
-  def activate
-    @course.activate
-    redirect_to @course, notice: 'Course was successfully activated.'
-  end
-
-  def deactivate
-    @course.deactivate
-    redirect_to @course, notice: 'Course was successfully deactivated.'
-  end
+  # def show
+  # end
+  #
+  # def new
+  #   @course = Course.new
+  # end
+  #
+  # def edit
+  # end
+  #
+  # def create
+  #   @course = Course.new(course_params)
+  #   if @course.save
+  #     redirect_to courses_path, notice: 'Course was successfully created.'
+  #   else
+  #     render :new
+  #   end
+  # end
+  #
+  # def update
+  #   if @course.update(course_params)
+  #     redirect_to @course, notice: 'Course was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
+  #
+  # def destroy
+  #   @course.trash
+  #   redirect_to courses_url, notice: 'Course was successfully destroyed.'
+  # end
+  #
+  # def activate
+  #   @course.activate
+  #   redirect_to @course, notice: 'Course was successfully activated.'
+  # end
+  #
+  # def deactivate
+  #   @course.deactivate
+  #   redirect_to @course, notice: 'Course was successfully deactivated.'
+  # end
 
   def select
     if params[:course] && params[:course][:id].to_i > 0
