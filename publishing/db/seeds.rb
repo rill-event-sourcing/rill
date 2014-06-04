b4d82049edb5e7ceabe5b98d3b3f83c035299623
Heliosmaster <helios89@gmail.com>
2014-06-04 10:07:57 +0200

    added seeds for the database with two course and two chapters each

--- publishing/db/seeds.rb
+++ publishing/db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Basic Layout with two courses
+
+math = Course.create(name: 'Math')
+english = Course.create(name: 'English')
+
+# For each course we put two chapters
+
+## Math
+algebra = Chapter.create(title: 'Algebra', description: 'A bunch of abstract things', course: math)
+geometry = Chapter.create(title: 'Geometry', description: 'A bunch of weird shapes', course: math)
+
+## English
+grammar = Chapter.create(title: 'Grammar', description: 'A bunch of strict rules', course: english)
+literature = Chapter.create(title: 'Literature', description: 'A bunch of words', course: english)
