2d9b9206bdbe8279c87a770c48de4c5e7a6accd4
Heliosmaster <helios89@gmail.com>
2014-06-04 12:10:53 +0200

    more meaningful seeds and description in sections

--- publishing/db/seeds.rb
+++ publishing/db/seeds.rb
@@ -1,14 +1,22 @@
 # Basic Layout with two courses
 
-math = Course.create(name: 'Math')
+math = Course.create(name: 'Counting')
 english = Course.create(name: 'English')
 
-# For each course we put two chapters
+# Two chapters
 
 ## Math
-algebra = Chapter.create(title: 'Algebra', description: 'A bunch of abstract things', course: math)
-geometry = Chapter.create(title: 'Geometry', description: 'A bunch of weird shapes', course: math)
+positional_system = Chapter.create(title: 'Positional system', description: 'Position matters', course: math)
+basic_operations = Chapter.create(title: 'Basic operations', description: 'Back to basics', course: math)
 
-## English
-grammar = Chapter.create(title: 'Grammar', description: 'A bunch of strict rules', course: english)
-literature = Chapter.create(title: 'Literature', description: 'A bunch of words', course: english)
+
+# For each chapter, we put two sections
+
+## Position system
+
+grouping_by_10 = Section.create(title: 'Grouping by 10', description: '10 by 10', chapter: positional_system)
+position_of_0 = Section.create(title: 'Position of the 0', description: 'at the back or at the front?', chapter: positional_system)
+
+## Basic operations
+addition = Section.create(title: 'Addition', description: '2+2=5 for the majority of values of 2', chapter: basic_operations)
+subtraction = Section.create(title: 'Subtraction', description: 'Too many things, let me get rid of some', chapter: basic_operations)
