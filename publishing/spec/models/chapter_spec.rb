cbb78cafd18c59b6050fb494b7d1fff4cdd48114
Steven Thonus <steven@ln2.nl>
2014-06-04 09:51:57 +0200

    fix

--- publishing/spec/models/chapter_spec.rb
+++ publishing/spec/models/chapter_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe Chapter, :type => :model do
   # it {is_expected.to have_many :sections}
 
   before do
-    build(:chapter, title: 'B', order: 2)
-    build(:chapter, title: 'C', order: 3)
-    build(:chapter, title: 'A', order: 1)
+    create(:chapter, title: 'B', order: 2)
+    create(:chapter, title: 'C', order: 3)
+    @chapter = create(:chapter, title: 'A', order: 1)
   end
 
   it "should return title when asked for its string" do
@@ -19,20 +19,20 @@ RSpec.describe Chapter, :type => :model do
   # it "should list chapters in order of order" do
   #   expect(Chapter.all.map(&:to_s)).to eq ['A', 'B', 'C']
   # end
-  #
-  # it "should not list trashed chapters" do
-  #   @chapter.trash
-  #   expect(Chapter.all.map(&:to_s)).to eq ['B', 'C']
-  #   expect(Chapter.trashed.first).to eq @chapter
-  # end
-  #
-  # it "should be activateable" do
-  #   @chapter = build(:chapter)
-  #   expect(@chapter.active).to eq false
-  #   @chapter.activate
-  #   expect(@chapter.active).to eq true
-  #   @chapter.deactivate
-  #   expect(@chapter.active).to eq false
-  # end
+
+  it "should not list trashed chapters" do
+    @chapter.trash
+    expect(Chapter.all.map(&:to_s)).to eq ['B', 'C']
+    expect(Chapter.trashed.first).to eq @chapter
+  end
+
+  it "should be activateable" do
+    @chapter = build(:chapter)
+    expect(@chapter.active).to eq false
+    @chapter.activate
+    expect(@chapter.active).to eq true
+    @chapter.deactivate
+    expect(@chapter.active).to eq false
+  end
 
 end
