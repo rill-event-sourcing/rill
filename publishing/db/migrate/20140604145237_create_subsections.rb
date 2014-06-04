e61c498db0b4e7f3d4340d7862a49e8caa9bcc32
Heliosmaster <helios89@gmail.com>
2014-06-04 16:54:40 +0200

    added empty models, view and controllers for subsections

---
+++ publishing/db/migrate/20140604145237_create_subsections.rb
@@ -0,0 +1,16 @@
+class CreateSubsections < ActiveRecord::Migration
+  def change
+    create_table :subsections do |t|
+        t.uuid :section_id, index: true
+        t.string :title
+        t.text :description
+        t.integer :level, limit: 1
+        t.datetime :deleted_at
+        t.boolean :active, default: false
+        t.integer :position, limit: 3
+        t.timestamps
+      end
+      add_index :sections, :created_at
+    end
+  end
+end
---
+++ publishing/spec/models/subsection_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Subsection, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
