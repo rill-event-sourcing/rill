51ce3a33b244eff8c02f0dff0fea7399caec244a
Steven Thonus <steven@ln2.nl>
2014-06-11 17:14:24 +0200

    added capistrano

--- publishing/config/deploy.rb
+++ publishing/config/deploy.rb
@@ -20,19 +20,14 @@ namespace :deploy do
 
   desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    on roles(:app) do
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
+  desc 'Compile assets'
+  task :compile_assets => [:set_rails_env] do
+    invoke 'deploy:assets:precompile'
   end
 
   # We have the Rails application in a subdirectory rails_app
@@ -46,5 +41,17 @@ namespace :deploy do
 
   after :finishing, 'deploy:cleanup'
   after 'deploy:updating', 'deploy:mv_rails_app_dir'
+  after 'deploy:updated', 'deploy:compile_assets'
 
+  namespace :assets do
+    task :precompile do
+      on roles(:web) do
+        within release_path do
+          with rails_env: fetch(:rails_env) do
+            execute :rake, "assets:precompile"
+          end
+        end
+      end
+    end
+  end
 end
