cad4d42f46ffd938f4430bcd03de67c1f6b5f5f7
Steven Thonus <steven@ln2.nl>
2014-06-11 14:02:42 +0200

    seperate javascript files

--- publishing/config/application.rb
+++ publishing/config/application.rb
@@ -26,6 +26,24 @@ module StudyflowPublishing
       #{config.root}/app/models/concerns
     )
 
+    # config.assets.precompile += ['sections.js']
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+          true
+        else
+          puts "excluding asset: " + full_path
+          false
+        end
+      else
+        false
+      end
+    end
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
