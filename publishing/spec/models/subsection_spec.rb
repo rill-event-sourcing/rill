0e878a09a04025cbd70ee9482561fc8e2ff0d051
Heliosmaster <helios89@gmail.com>
2014-06-11 14:54:17 +0200

    added tests for subsection model

--- publishing/spec/models/subsection_spec.rb
+++ publishing/spec/models/subsection_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Subsection, :type => :model do
+  it {is_expected.to validate_presence_of :section }
+
+  before do
+    create(:subsection, title: "A", description: "A content", stars: 1)
+    create(:subsection, title: "B", description: "B content", stars: 2)
+    @subsection = create(:subsection, title: "C", description: "C content", stars: 3)
+  end
+
+  it "should return the title when asked for a string" do
+    @subsection = build(:subsection)
+    expect(@subsection.to_s).to eq @subsection.title
+  end
+
+  it "should return an abbreviated uuid" do
+    @subsection = create(:subsection)
+    id = @subsection.id.to_s
+    expect(@subsection.to_param).to eq id[0..7]
+  end
+
+  it "should return a json object" do
+    obj = {id: @subsection.id, title: @subsection.title}
+    expect(@subsection.as_json).to eq obj
+  end
 
 end
