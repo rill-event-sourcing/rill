3f126879077b9507920c6135a3096ea7f90920f8
Steven Thonus <steven@ln2.nl>
2014-06-11 11:16:32 +0200

    added add button

--- publishing/app/models/chapter.rb
+++ publishing/app/models/chapter.rb
@@ -13,9 +13,9 @@ class Chapter < ActiveRecord::Base
 
   scope :for_short_uuid, ->(id) { where(["SUBSTRING(CAST(id AS VARCHAR), 1, 8) = ?", id]) }
 
-  def self.find_by_uuid(id)
+  def self.find_by_uuid(id, with_404 = true)
     chapters = for_short_uuid(id)
-    raise ActiveRecord::RecordNotFound if chapters.empty?
+    raise ActiveRecord::RecordNotFound if chapters.empty? && with_404
     raise StudyflowPublishing::ShortUuidDoubleError.new("Multiple chapters found for uuid: #{id}") if chapters.length > 1
     chapters.first
   end
