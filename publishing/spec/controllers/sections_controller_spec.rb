a49a72e6feb46c3273477fa5dce2fbb6a3f8a6fc
Steven Thonus <steven@ln2.nl>
2014-06-12 16:02:08 +0200

    add sectionscontroller test

--- publishing/spec/controllers/sections_controller_spec.rb
+++ publishing/spec/controllers/sections_controller_spec.rb
@@ -7,9 +7,10 @@ RSpec.describe SectionsController, :type => :controller do
     session[:course_id] = @course.id
     controller.set_my_course
     @chapter = create(:chapter, course: @course)
-    @section1 = create(:section, chapter: @chapter, position: 0)
-    @section2 = create(:section, chapter: @chapter, position: 1)
-    @section3 = create(:section, chapter: @chapter, position: 2)
+
+    @section1 = create(:section, chapter: @chapter)
+    @section2 = create(:section, chapter: @chapter)
+    @section3 = create(:section, chapter: @chapter)
   end
 
   describe "GET index" do
@@ -51,6 +52,7 @@ RSpec.describe SectionsController, :type => :controller do
 
 
 
+
   describe "POST activate" do
     it "should activate the section and redirect" do
       post :activate, chapter_id: @chapter.id[0,8], id: @section1.id[0,8]
@@ -67,22 +69,26 @@ RSpec.describe SectionsController, :type => :controller do
     end
   end
 
-  # describe "POST moveup" do
-  #   it "should moveup the section and redirect" do
-  #     expect(@section2.position).to eq 1
-  #     # post :moveup, chapter_id: @chapter.id[0,8], id: @section2.id[0,8]
-  #     # expect(response).to redirect_to chapter_sections_path
-  #     # expect(@section2.position).to eq 0
-  #   end
-  # end
-  #
-  # describe "POST movedown" do
-  #   it "should movedown the section and redirect" do
-  #     expect(@section1.position).to eq 0
-  #     # post :movedown, chapter_id: @chapter.id[0,8], id: @section1.id[0,8]
-  #     # expect(response).to redirect_to chapter_sections_path
-  #     # expect(@section1.position).to eq 1
-  #   end
-  # end
+  describe "POST moveup" do
+    it "should moveup the section and redirect" do
+      expect(@section2.position).to eq 2
+      post :moveup, chapter_id: @chapter.id[0,8], id: @section2.id[0,8]
+      expect(assigns(:section)).to eq @section2
+      expect(response).to redirect_to chapter_sections_path
+      @section2.reload
+      expect(@section2.position).to eq 1
+    end
+  end
+
+  describe "POST movedown" do
+    it "should movedown the section and redirect" do
+      expect(@section2.position).to eq 2
+      post :movedown, chapter_id: @chapter.id[0,8], id: @section2.id[0,8]
+      expect(assigns(:section)).to eq @section2
+      expect(response).to redirect_to chapter_sections_path
+      @section2.reload
+      expect(@section2.position).to eq 3
+    end
+  end
 
 end
