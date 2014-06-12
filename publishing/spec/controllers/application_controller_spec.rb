da4f7c0718922d9ab8e17d1755578a8037aa57c4
Steven Thonus <steven@ln2.nl>
2014-06-12 15:06:52 +0200

    add applicationcontroller test

---
+++ publishing/spec/controllers/application_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, :type => :controller do
+
+  before do
+    @course = create(:course, name: 'Math', active: true)
+  end
+
+  describe "helper methods" do
+    it "should set_my_course and rest_my_course" do
+      session[:course_id] = @course.id
+      expect(Course.current).to eq nil
+      controller.set_my_course
+      expect(Course.current).to eq @course
+
+      session[:course_id] = nil
+      controller.reset_my_course
+      expect(Course.current).to eq nil
+    end
+
+    it "should set_crumb" do
+      @crumbs = nil
+
+      crumb_hash1 = {link: '1234'}
+      crumbs = controller.set_crumb(crumb_hash1)
+      expect(assigns(:crumbs)).to eq [crumb_hash1]
+
+      crumb_hash2 = {link2: '5678'}
+      controller.set_crumb(crumb_hash2)
+      expect(assigns(:crumbs)).to eq [crumb_hash1, crumb_hash2]
+    end
+  end
+
+end
