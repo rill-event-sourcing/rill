c83074ee2118dce9d79d606715045f57bb83480a
Joost Diepenmaat <joost@zeekat.nl>
2014-06-12 14:59:21 +0200

    Move tooling and middleware into their own namespaces.

--- learning/server/src/studyflow/web/api.clj
+++ learning/server/src/studyflow/web/api.clj
@@ -1,27 +1,20 @@
 (ns studyflow.web.api
   (:require [clout-link.route :refer [handle]]
             [rill.uuid :refer [new-id]]
-            [ring.middleware.json :refer [wrap-json-body
-                                          wrap-json-response]]
-            [studyflow.json-tools :refer [key-to-json key-from-json]]
+            [studyflow.learning.commands :as commands]
             [studyflow.learning.course-material :as material]
             [studyflow.learning.read-model :as read-model]
             [studyflow.web.command-executor :refer [wrap-command-executor]]
+            [studyflow.web.handler-tools :refer [combine-ring-handlers]]
+            [studyflow.web.json-middleware :refer [wrap-json-io]]
             [studyflow.web.logging :refer [wrap-logging]]
-            [studyflow.web.routes :as routes]
-            [studyflow.learning.commands :as commands]))
+            [studyflow.web.routes :as routes]))
 
 (defn wrap-middleware
   [f]
   (-> f
-      (wrap-json-response {:key-fn key-to-json})
-      (wrap-json-body {:keywords? key-from-json})
+      wrap-json-io
       wrap-logging))
-
-(defn combine-ring-handlers
-  [& handlers]
-  (fn [r]
-    (some #(% r) handlers)))
 
 (def command-ring-handler
   "This handler matches ring requests and returns a command (or nil) for the given request.
@@ -39,15 +32,15 @@ commands."
            (fn [{model :read-model {course-id :course-id} :params}]
              (read-model/course-tree model course-id)))))
 
-
 (defn wrap-read-model
-  [event-store]
-  ()
-  )
+  [f read-model-atom]
+  (fn [r]
+    (f (assoc r :read-model @read-model-atom))))
 
 (defn make-request-handler
-  [event-store]
+  [event-store read-model]
   (-> #'command-ring-handler
       (wrap-command-executor event-store)
       query-handler
+      (wrap-read-model read-model)
       wrap-middleware))
