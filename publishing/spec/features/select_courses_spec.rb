bc05056b6c80b17c050c684816acdb203f7e0b4d
Steven Thonus <steven@ln2.nl>
2014-06-12 14:39:08 +0200

    add coursecontroller test

--- publishing/spec/features/select_courses_spec.rb
+++ publishing/spec/features/select_courses_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
-feature "SelectCourses", :type => :feature do
+feature "SelectCourses", type: :feature do
   before do
     create(:course, name: 'Math', active: true)
     create(:course, name: 'Engels', active: true)
   end
 
-  scenario 'Course selection', js: true do
+  scenario 'do Course selection', js: true do
     visit root_path
-    expect(page).to have_select('course_id', options: ['choose course', 'Math', 'Engels'])
-    select('Math', :from => 'course_id')
+    expect(page).to have_select('course_id', options: ['choose course', 'Engels', 'Math'])
+    select('Math', from: 'course_id')
     visit root_path
-    expect(page).to have_select('course_id', :selected => 'Math')
+    expect(page).to have_select('course_id', selected: 'Math')
+    select('choose course', :from => 'course_id')
+    visit root_path
+    expect(page).to have_select('course_id', options: ['choose course', 'Engels', 'Math'])
+    expect(Course.current).to eq nil
   end
 
 end
