23c3d194822516e4d129f08d92843664c506762a
Steven Thonus <steven@ln2.nl>
2014-06-12 17:29:26 +0200

    adding subsections test

--- publishing/spec/controllers/subsections_controller_spec.rb
+++ publishing/spec/controllers/subsections_controller_spec.rb
@@ -2,4 +2,32 @@ require 'rails_helper'
 
 RSpec.describe SubsectionsController, :type => :controller do
 
+  before do
+    @course = create(:course)
+    session[:course_id] = @course.id
+    controller.set_my_course
+    @chapter = create(:chapter, course: @course)
+
+    @section1 = create(:section, chapter: @chapter)
+    @section2 = create(:section, chapter: @chapter)
+    @section3 = create(:section, chapter: @chapter)
+
+    # @subsection1 = create(:subsection, section: @section1, stars: 1)
+    # @subsection2 = create(:subsection, section: @section1, stars: 2)
+    # @subsection3 = create(:subsection, section: @section1, stars: 2)
+  end
+
+  describe "POST create" do
+    it "should create a new subsection" do
+      post :create, chapter_id: @chapter.id[0,8], section: {title: "new section"}
+      expect(response).to redirect_to chapter_section_path(@chapter, assigns(:section))
+    end
+    it "should not create a invalid subsection" do
+      post :create, chapter_id: @chapter.id[0,8], section: {title: ""}
+      expect(response).to render_template('new')
+    end
+  end
+
+  
+
 end
