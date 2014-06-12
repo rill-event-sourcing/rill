380b446f2acd6e5f60b7bc7ec1f8e2202b4a5772
Heliosmaster <helios89@gmail.com>
2014-06-12 12:30:48 +0200

    added controller specs (empty)

--- publishing/config/application.rb
+++ publishing/config/application.rb
@@ -15,7 +15,7 @@ module StudyflowPublishing
         view_specs: false,
         helper_specs: false,
         routing_specs: false,
-        controller_specs: false,
+        controller_specs: true,
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     end
