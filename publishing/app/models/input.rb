165891080288d492534feb1ee1a52d4ebf4020fb
Heliosmaster <helios89@gmail.com>
2014-06-18 17:16:47 +0200

    it works!

--- publishing/app/models/input.rb
+++ publishing/app/models/input.rb
@@ -1,4 +1,9 @@
 class Input < ActiveRecord::Base
   belongs_to :question
   validates :question, presence: true
+
+  def to_param
+    "#{id[0,8]}"
+  end
+  
 end
--- publishing/spec/factories/line_inputs.rb
+++ publishing/spec/factories/line_inputs.rb
@@ -2,7 +2,6 @@
 
 FactoryGirl.define do
   factory :line_input do
-    text "Default question text"
     question
   end
 end
