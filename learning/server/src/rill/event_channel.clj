4b92a9b9a9a676b6f6651eff280805d8f243874c
Joost Diepenmaat <joost@zeekat.nl>
2014-06-18 07:56:55 +0200

    Refactor event channel handler.

--- learning/server/src/rill/event_channel.clj
+++ learning/server/src/rill/event_channel.clj
@@ -5,23 +5,33 @@
 
 (def long-poll-seconds 20)
 
+(defn push-event!!
+  "push event to channel. ignores nil events.
+returns false when we should not continue pushing."
+  [ch event]
+  (log/info event)
+  (if event
+    (boolean (>!! ch event))
+    true))
+
+(defn push-events!!
+  [ch events]
+  (loop [[event & events] events]
+    (if (seq events)
+      (if (push-event!! ch event)
+        (recur events)
+        false)
+      true)))
+
 (defn event-channel-listen!!
   "Push events from stream into ch. Blocking"
   [event-store stream-id from-version ch]
-  (log/debug [:listen!! stream-id from-version])
-  (let [[continue? to-version]
-        (reduce
-         (fn [[continue? version] event]
-           (log/info event)
-           (if event
-             (if (>!! ch event)
-               [true (inc version)]
-               [false version])
-             [true version]))
-         [true from-version]
-         (store/retrieve-events-since event-store stream-id from-version long-poll-seconds))]
-    (when continue?
-      (recur event-store stream-id to-version ch))))
+  (loop [version from-version]
+    (log/debug [:listen!! stream-id version])
+    (let [events (store/retrieve-events-since event-store stream-id version long-poll-seconds)]
+      (log/debug ["recieved" (count events) "events"])
+      (when (push-events!! ch events)
+        (recur (+ version (count events)))))))
 
 (defn event-channel
   "Start an event listener in a new thread and returns a channel
