4f7e9605f1bdd54cad71fc6bcdea7792342db351
Steven Thonus <steven@ln2.nl>
2014-06-19 10:53:39 +0200

    add ruby-version; remove question type column

--- publishing/db/migrate/20140618075731_create_questions.rb
+++ publishing/db/migrate/20140618075731_create_questions.rb
@@ -2,7 +2,6 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions, id: :uuid do |t|
       t.uuid :section_id, index: true
-      t.string :type
       t.text :text
       t.datetime :deleted_at, index: true
       t.boolean :active, default: false
