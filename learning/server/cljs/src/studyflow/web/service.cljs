c0ed25ae45f034a64f3f221637fd4c7480955924
thegeez <thegeez@users.noreply.github.com>
2014-06-19 14:37:34 +0200

    Use Om component for cljs service to participate in om's state management

--- learning/server/cljs/src/studyflow/web/service.cljs
+++ learning/server/cljs/src/studyflow/web/service.cljs
@@ -7,45 +7,7 @@
 ;; a bit silly to use an Om component for something that is not UI,
 ;; but don't know how to participate in state managament otherwise
 (defn start-service [app-state]
-  (.setTimeout js/window
-          #(GET (str "/api/course-material/"
-                    (get-in @app-state [:course-id]))
-               {:params {}
-                :handler (fn [res]
-                           (println "Service heard: " res)
-                           (let [course-data (json-edn/json->edn res)]
-                             (swap! app-state assoc :course-material course-data)))
-                :error-handler (fn [res]
-                                 (println "Error handler" res)
-                                 (println res))})
-          1000)
-  (add-watch app-state :service
-             (fn [k a old new]
-               (if-let [[chapter-id section-id] (get-in new [:selected-section])]
-                 (if-let [section-data (get-in new [:sections-data section-id])]
-                   nil ;; data already loaded
-                   (GET (str "/api/course-material/"
-                             (get-in new [:course-id])
-                             "/chapter/" chapter-id
-                             "/section/" section-id)
-                        {:params {}
-                         :handler (fn [res]
-                                    (println "Service heard: " res)
-                                    (let [section-data (json-edn/json->edn res)]
-                                      (println "section: " section-data)
-                                      (swap! app-state assoc-in [:sections-data (:id section-data)]
-                                             section-data)))
-                         :error-handler (fn [res]
-                                          (println "Error handler" res)
-                                          (println res))}))
-                 nil ;; maybe preload here?
-                 )))
-  
-  
-  ;; check if there can be a listen on the cursor for all changes, so
-  ;; we can use tx-listen with two args rather than the add-watch on
-  ;; the atom
-  #_(om/root
+  (om/root
    (fn [cursor owner]
      (reify
        om/IRender
@@ -66,5 +28,30 @@
                                  (println res))})
           1000))))
    app-state
-   {:target (. js/document (getElementById "services"))}))
+   {:target (. js/document (getElementById "services"))
+    :tx-listen (fn [tx-report cursor]
+                 (let [{:keys [path new-state]} tx-report]
+                   (cond
+                    (= path [:selected-section])
+                    (if-let [[chapter-id section-id] (get-in new-state [:selected-section])]
+                      (if-let [section-data (get-in new-state [:sections-data section-id])]
+                       nil ;; data already loaded
+                       (GET (str "/api/course-material/"
+                                 (get-in new-state [:course-id])
+                                 "/chapter/" chapter-id
+                                 "/section/" section-id)
+                            {:params {}
+                             :handler (fn [res]
+                                        (println "Service heard: " res)
+                                        (let [section-data (json-edn/json->edn res)]
+                                          (println "section: " section-data)
+                                          (om/transact! cursor
+                                                        #(assoc-in %
+                                                                   [:sections-data (:id section-data)]
+                                                                   section-data))))
+                              :error-handler (fn [res]
+                                               (println "Error handler" res)
+                                               (println res))}))
+                     nil ;; maybe preload here?
+                     ))))}))
 
