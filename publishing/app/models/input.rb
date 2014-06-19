aab80dc3eb390aec48d0107deccf946d1f001859
Steven Thonus <steven@ln2.nl>
2014-06-19 16:12:10 +0200

    setup edit question page

--- publishing/app/models/input.rb
+++ publishing/app/models/input.rb
@@ -6,4 +6,12 @@ class Input < ActiveRecord::Base
     "#{id[0,8]}"
   end
 
+  def line_input?
+    is_a?(LineInput)
+  end
+
+  def multiple_choice_input?
+    is_a?(MultipleChoiceInput)
+  end
+
 end
