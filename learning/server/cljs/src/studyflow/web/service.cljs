0bfe32c1e230501bdfea66336da3149dd4e56f16
thegeez <thegeez@users.noreply.github.com>
2014-06-19 15:32:50 +0200

    Code layout

--- learning/server/cljs/src/studyflow/web/service.cljs
+++ learning/server/cljs/src/studyflow/web/service.cljs
@@ -19,17 +19,17 @@
        om/IWillMount
        (will-mount [_]
          (.setTimeout js/window
-          #(GET (str "/api/course-material/"
-                    (get-in @cursor [:course-id]))
-               {:params {}
-                :handler (fn [res]
-                           (println "Service heard: " res)
-                           (let [course-data (json-edn/json->edn res)]
-                             (om/update! cursor :course-material course-data)))
-                :error-handler (fn [res]
-                                 (println "Error handler" res)
-                                 (println res))})
-          1000))))
+                      #(GET (str "/api/course-material/"
+                                 (get-in @cursor [:course-id]))
+                            {:params {}
+                             :handler (fn [res]
+                                        (println "Service heard: " res)
+                                        (let [course-data (json-edn/json->edn res)]
+                                          (om/update! cursor :course-material course-data)))
+                             :error-handler (fn [res]
+                                              (println "Error handler" res)
+                                              (println res))})
+                      1000))))
    app-state
    {:target (. js/document (getElementById "services"))
     :tx-listen (fn [tx-report cursor]
@@ -38,23 +38,23 @@
                     (= path [:selected-section])
                     (if-let [[chapter-id section-id] (get-in new-state [:selected-section])]
                       (if-let [section-data (get-in new-state [:sections-data section-id])]
-                       nil ;; data already loaded
-                       (GET (str "/api/course-material/"
-                                 (get-in new-state [:course-id])
-                                 "/chapter/" chapter-id
-                                 "/section/" section-id)
-                            {:params {}
-                             :handler (fn [res]
-                                        (println "Service heard: " res)
-                                        (let [section-data (json-edn/json->edn res)]
-                                          (println "section: " section-data)
-                                          (om/transact! cursor
-                                                        #(assoc-in %
-                                                                   [:sections-data (:id section-data)]
-                                                                   section-data))))
+                        nil ;; data already loaded
+                        (GET (str "/api/course-material/"
+                                  (get-in new-state [:course-id])
+                                  "/chapter/" chapter-id
+                                  "/section/" section-id)
+                             {:params {}
+                              :handler (fn [res]
+                                         (println "Service heard: " res)
+                                         (let [section-data (json-edn/json->edn res)]
+                                           (println "section: " section-data)
+                                           (om/transact! cursor
+                                                         #(assoc-in %
+                                                                    [:sections-data (:id section-data)]
+                                                                    section-data))))
                               :error-handler (fn [res]
                                                (println "Error handler" res)
                                                (println res))}))
-                     nil ;; maybe preload here?
-                     ))))}))
+                      nil ;; maybe preload here?
+                      ))))}))
 
