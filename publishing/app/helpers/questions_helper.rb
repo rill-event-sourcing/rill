b8f862ff0bbe0fc632a67c57d7597850ff05a941
Steven Thonus <steven@ln2.nl>
2014-06-20 12:55:19 +0200

    pre render choices and answers for question preview

--- publishing/app/helpers/questions_helper.rb
+++ publishing/app/helpers/questions_helper.rb
@@ -1,2 +1,23 @@
 module QuestionsHelper
+
+  def question_to_html(question)
+    html = content_tag(:div, question.text)
+    question.inputs.each do |input|
+      html << input_to_html(input)
+    end
+    html
+  end
+
+  def input_to_html(input)
+    if input.line_input?
+      content_tag(:input)
+    elsif input.multiple_choice_input?
+      content_tag(:ul) do
+        input.choices.map{|ch| content_tag(:li, ch.value) }.join('').html_safe
+      end
+    else
+      content_tag(:div, 'unknown input type')
+    end
+  end
+
 end
