23154bfe27ed32387cba931240c3a57832c80a54
thegeez <thegeez@users.noreply.github.com>
2014-06-23 10:02:19 +0200

    Revert "Try alternative react with quiescent"
    This reverts commit 5a9334bc3303c7740052fad66534c0b0a23878db.

--- learning/server/cljs/src/studyflow/web/core.cljs
+++ learning/server/cljs/src/studyflow/web/core.cljs
@@ -2,10 +2,7 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [studyflow.web.service :as service]
-            [studyflow.web.qservice :as qservice]
-            [clojure.string :as string]
-            [quiescent :as q :include-macros true]
-            [quiescent.dom :as qdom]))
+            [clojure.string :as string]))
 
 
 (enable-console-print!)
@@ -64,55 +61,5 @@
    content
    app-state
    {:target (. js/document (getElementById "content"))})
+  (.log js/console "Here")
   (service/start-service app-state))
-
-
-(def qstate (atom {:course-id  (course-id-for-page)}))
-
-(q/defcomponent Navigation [state qstate]
-  (if-let [course (:course-material state)]
-    (qdom/div {} "Course-material available: "
-              (qdom/h1 {:data-id (:id course)} (:name course))
-              (apply qdom/ul nil
-                     (for [{:keys [title sections]
-                            chapter-id :id
-                            :as chapter} (:chapters course)]
-                       (qdom/li #js {:data-id chapter-id}
-                                title
-                                (apply qdom/ul nil
-                                       (for [{:keys [title]
-                                              section-id :id
-                                              :as section} sections]
-                                         (qdom/a #js {:onClick
-                                                      (fn [_]
-                                                        (println "onclick" (pr-str state))
-                                                        (swap! qstate
-                                                               assoc :selected-section
-                                                               [chapter-id section-id]
-                                                               :event [:navigation-selection]))}
-                                                 (dom/li #js {:data-id section-id}
-                                                         title))))))))
-    (qdom/h1 {} "No navigation available")))
-
-(q/defcomponent Content [state]
-  (if-let [[_ section-id] (get-in state [:selected-section])]
-    (if-let [section-data (get-in state [:sections-data section-id])]
-      (dom/div nil (str "Section data:" (pr-str section-data)))
-      (dom/h2 nil (str "Selected data not yet loaded" section-id)))
-    (dom/h2 nil "No section selected")))
-
-(q/defcomponent App
-  [state]
-  (qdom/div {}
-            (Navigation state qstate)
-            (Content state)))
-
-(defn ^:export course-page-quiescent []
-  (.log js/console "Hello quiescent")
-  (add-watch qstate :quiescent
-             (fn [_ _ old state]
-               ;; Queue with animationFrame, through goog closure
-               (q/render (App state qstate)
-                         (.getElementById js/document "content"))))
-  (qservice/start-service qstate)
-  (swap! qstate identity))
