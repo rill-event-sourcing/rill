c710913c7c4f8b9ebcea489f3a352f8796b78392
thegeez <thegeez@users.noreply.github.com>
2014-06-23 10:30:15 +0200

    Service as the root component is the default pattern for om

--- learning/server/cljs/src/studyflow/web/service.cljs
+++ learning/server/cljs/src/studyflow/web/service.cljs
@@ -6,55 +6,54 @@
 
 ;; a bit silly to use an Om component for something that is not UI,
 ;; but don't know how to participate in state managament otherwise
-(defn start-service [app-state]
-  (om/root
-   (fn [cursor owner]
-     (reify
-       om/IShouldUpdate
-       (should-update [this next-props next-state]
-         false)
-       om/IRender
-       (render [_]
-         (dom/div nil nil))
-       om/IWillMount
-       (will-mount [_]
-         (.setTimeout js/window
-                      #(GET (str "/api/course-material/"
-                                 (get-in @cursor [:course-id]))
-                            {:params {}
-                             :handler (fn [res]
-                                        (println "Service heard: " res)
-                                        (let [course-data (json-edn/json->edn res)]
-                                          (om/update! cursor :course-material course-data)))
-                             :error-handler (fn [res]
-                                              (println "Error handler" res)
-                                              (println res))})
-                      1000))))
-   app-state
-   {:target (. js/document (getElementById "services"))
-    :tx-listen (fn [tx-report cursor]
-                 (let [{:keys [path new-state]} tx-report]
-                   (cond
-                    (= path [:selected-section])
-                    (if-let [[chapter-id section-id] (get-in new-state [:selected-section])]
-                      (if-let [section-data (get-in new-state [:sections-data section-id])]
-                        nil ;; data already loaded
-                        (GET (str "/api/course-material/"
-                                  (get-in new-state [:course-id])
-                                  "/chapter/" chapter-id
-                                  "/section/" section-id)
-                             {:params {}
-                              :handler (fn [res]
-                                         (println "Service heard: " res)
-                                         (let [section-data (json-edn/json->edn res)]
-                                           (println "section: " section-data)
-                                           (om/transact! cursor
-                                                         #(assoc-in %
-                                                                    [:sections-data (:id section-data)]
-                                                                    section-data))))
-                              :error-handler (fn [res]
-                                               (println "Error handler" res)
-                                               (println res))}))
-                      nil ;; maybe preload here?
-                      ))))}))
+;; root component for services seems to be the default om pattern:
+;; https://groups.google.com/forum/#!topic/clojurescript/DHJvcGey8Sc
 
+(defn wrap-service [widgets]
+  (fn [cursor owner]
+    (reify
+      om/IWillMount
+      (will-mount [_]
+        (println "service will mount")
+        ;; listen to server push connection here
+        (.setTimeout js/window
+                     #(GET (str "/api/course-material/"
+                                (get-in @cursor [:course-id]))
+                           {:params {}
+                            :handler (fn [res]
+                                       (println "Service heard: " res)
+                                       (let [course-data (json-edn/json->edn res)]
+                                         (om/update! cursor :course-material course-data)))
+                            :error-handler (fn [res]
+                                             (println "Error handler" res)
+                                             (println res))})
+                     1000))
+      om/IRender
+      (render [_]
+        (om/build widgets cursor)))))
+
+(defn listen [tx-report cursor]
+  (let [{:keys [path new-state]} tx-report]
+    (cond
+     (= path [:selected-section])
+     (if-let [[chapter-id section-id] (get-in new-state [:selected-section])]
+       (if-let [section-data (get-in new-state [:sections-data section-id])]
+         nil ;; data already loaded
+         (GET (str "/api/course-material/"
+                   (get-in new-state [:course-id])
+                   "/chapter/" chapter-id
+                   "/section/" section-id)
+              {:params {}
+               :handler (fn [res]
+                          (println "Service heard: " res)
+                          (let [section-data (json-edn/json->edn res)]
+                            (println "section: " section-data)
+                            (om/transact! cursor
+                                          #(assoc-in %
+                                                     [:sections-data (:id section-data)]
+                                                     section-data))))
+               :error-handler (fn [res]
+                                (println "Error handler" res)
+                                (println res))}))
+       nil ;; maybe preload here?
+       ))))
