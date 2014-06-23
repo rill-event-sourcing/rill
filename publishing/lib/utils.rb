3e14710a0323c2b8824bbcdb434845e40b97afbe
Davide Taviani <helios89@gmail.com>
2014-06-23 12:44:26 +0200

    type instead of soort

--- publishing/lib/utils.rb
+++ publishing/lib/utils.rb
@@ -1,19 +1,19 @@
-def pp(value = '', soort = 'debug', debug_start = false)
+def pp(value = '', type = 'debug', debug_start = false)
   start = Time.now
   logger ||= ::Rails.logger
-  logger.send(soort, "\e[46;1;31m")
+  logger.send(type, "\e[46;1;31m")
   if @debug_date_time
     txt = "===> ∂: #{ (start - @debug_date_time).round(2) } #{ value.inspect } ===="
     p txt if Rails.env != 'production'
-    logger.send(soort, txt)
+    logger.send(type, txt)
   else
     txt = "===> #{ start.strftime("%H:%M:%S") }.#{ start.tv_sec } ============================================================="
-    logger.send(soort, txt)
-    logger.send(soort, "\e[1;37m")
-    logger.send(soort, "===> #{ value.inspect }")
-    logger.send(soort, "\e[1;31m")
-    logger.send(soort, "===> =================================================================================")
+    logger.send(type, txt)
+    logger.send(type, "\e[1;37m")
+    logger.send(type, "===> #{ value.inspect }")
+    logger.send(type, "\e[1;31m")
+    logger.send(type, "===> =================================================================================")
   end
-  logger.send(soort, "\e[44;0;30m");
+  logger.send(type, "\e[44;0;30m");
   @debug_date_time = start if debug_start
 end
