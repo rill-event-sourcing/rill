4b5accb0dee0d7c5512b11240372303c99ee179a
Remco van 't Veer <remco@remworks.net>
2014-06-25 16:43:35 +0200

    Remove unused stuff

--- learning/server/src/studyflow/web/browser_resources.clj
+++ learning/server/src/studyflow/web/browser_resources.clj
@@ -1,29 +1,14 @@
 (ns studyflow.web.browser-resources
-  (:require [clojure.tools.logging :refer [info debug spy]]
-            [clout-link.route :refer [handle]]
-            [studyflow.learning.read-model.queries :as queries]
+  (:require [clout-link.route :refer [handle]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.util.response :refer [resource-response]]
             [studyflow.web.handler-tools :refer [combine-ring-handlers]]
-            [studyflow.web.routes :as routes]
-            [rill.uuid :refer [uuid]]
-            [clojure.java.io :as io]
-            [ring.middleware.resource :as resource]))
-
-
-(defn course-page-handler
-  [{model :read-model {course-id :course-id :as params} :params}]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body (slurp (io/resource "templates/courses.html"))})
-
-(defn wrap-read-model
-  [f read-model-atom]
-  (fn [r]
-    (f (assoc r :read-model @read-model-atom))))
+            [studyflow.web.routes :as routes]))
 
 (defn make-request-handler
   [read-model]
   (-> (combine-ring-handlers
        (handle routes/get-course-page
-               course-page-handler))
-      (wrap-read-model read-model)
-      (resource/wrap-resource "/")))
+               (constantly
+                (resource-response "templates/courses.html"))))
+      (wrap-resource "/")))
