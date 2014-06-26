52e66bb2ab5d5e597fd796cd657ddecd0044222b
Steven Thonus <steven@ln2.nl>
2014-06-26 11:03:35 +0200

    start on login form and validation

--- login/src/studyflow/login/main.clj
+++ login/src/studyflow/login/main.clj
@@ -1,22 +1,25 @@
 (ns studyflow.login.main
   (:require [clojure.java.jdbc :as sql]
             [clojure.string :as str]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [compojure.route :refer [not-found]]
             [environ.core :refer [env]]
             [hiccup.page :refer [html5 include-css]]
+            [hiccup.form :as form]
+            [ring.util.response :as response]
+            [ring.middleware.session :as session]
+            [ring.middleware.anti-forgery :as anti-forgery]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; View
 
-(def app-title "Studyflow login")
+(def app-title "Studyflow")
 
 (defn layout [title & body]
   (html5
    [:head
-    [:title (str/join "-" [app-title title])]
+    [:title (str/join " - " [app-title title])]
     (include-css "screen.css")]
    [:body
     [:h1 title]
@@ -30,6 +33,19 @@
    [:div
     (str/join " ," user-list)]])
 
+(defn login [email password msg]
+  (form/form-to [:post "/login"]
+    (form/hidden-field "__anti-forgery-token" anti-forgery/*anti-forgery-token*)
+    [:div
+      [:p msg]
+      [:div
+        (form/label "email" "email")
+        (form/email-field "email" email)]
+      [:div
+        (form/label "password" "password")
+        (form/password-field "password" password)]
+      [:div
+        (form/submit-button "login")]]))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Model
@@ -40,20 +56,41 @@
     (sql/query db "SELECT COUNT(*) FROM users"))))
 
 (defn list-users [db]
-  (map :uuid  (sql/query db "SELECT * FROM users")))
+  (let [result  (sql/query db "SELECT * FROM users")]
+   (clojure.string/join ", " (map :uuid result) )))
+
+(defn authenticated? [email,password]
+  ;; check database
+  (and (= email "info@studyflow.nl") (= password "beard")))
+
+(defn persisted? [email]
+  ;; check session
+  (= 1 2))
+
+(defn persist! [email]
+  ;; set session persistent
+  (session/wrap-session))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Controller
 
 (defroutes actions
   (GET "/" {db :db}
-       (layout "HOME" (home (count-users db) (list-users db))))
+    (if
+      (persisted? "info@studyflow.nl")
+        (layout "home" (home (count-users db) (list-users db)))
+        (response/redirect "/login")))
+  (GET "/login" {db :db}
+    (layout "login" (login nil nil "please login")))
+  (POST "/login" [email password]
+    (if
+      (authenticated? email password)
+        (response/redirect "/")
+        (layout "login" (login email password "wrong email / password combination"))))
   (not-found "Nothing here"))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Wiring
-
 
 (defn wrap-db [app db]
   (fn [req]
