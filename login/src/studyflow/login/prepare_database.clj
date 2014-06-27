2c954b08df58a430dda457fdbe31541a5f411b4b
Davide Taviani <helios89@gmail.com>
2014-06-27 17:42:07 +0200

    Tables are manipulated, not the whole database

--- login/src/studyflow/login/prepare_database.clj
+++ login/src/studyflow/login/prepare_database.clj
@@ -1,24 +1,24 @@
-(ns studyflow.login.prepare-database
+(ns studyflow.login.prepare-table
  (:require [clojure.java.jdbc :as sql]
   :require [studyflow.login.main :as login]))
 
-(defn seed-database [db]
+(defn seed-table [db]
   (login/create-user db "student", "student@studyflow.nl" "student")
   (login/create-user db "coach", "coach@studyflow.nl" "coach")
   (login/create-user db "editor", "editor@studyflow.nl" "editor")
   (login/create-user db "tester", "tester@studyflow.nl" "tester"))
  
-(defn create-database [db]
+(defn create-table [db]
   (sql/execute! db [(str "CREATE TABLE IF NOT EXISTS users (uuid VARCHAR(36) PRIMARY KEY, "
          "role VARCHAR(16) NOT NULL, "
          "email VARCHAR(255) NOT NULL, "
          "password VARCHAR(255) NOT NULL"
          ")")]))
 
-(defn clean-database [db]
-    (sql/execute! db [(str "DROP TABLE users")]))
+(defn clean-table [db]
+    (sql/execute! db ["TRUNCATE users"]))
 
 (defn -main []
-  (clean-database login/db)
-  (create-database login/db)
-  (seed-database login/db))
+  (create-table login/db)
+  (clean-table login/db)
+  (seed-table login/db))
