61880828edbfcad1ed018d95cf5ed09af1685a58
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-06-30 16:07:42 +0200

    Make course page work again with events as maps rather than records

--- learning/server/src/studyflow/learning/command_handler.clj
+++ learning/server/src/studyflow/learning/command_handler.clj
@@ -1,5 +1,6 @@
 (ns studyflow.learning.command-handler
-  (:require [studyflow.events :as evt]
+  (:require [clojure.tools.logging :as log]
+            [studyflow.events :as evt]
             [rill.message :as m]
             [studyflow.learning.commands :as commands]
             [studyflow.learning.write-model]
@@ -15,15 +16,15 @@
   (if course
     [(evt/map->CourseUpdated (-> command
                                  (select-keys [:course-id :material])
-                                 (assoc :m/id (new-id))))]
+                                 (assoc ::m/id (new-id))))]
     [(evt/map->CoursePublished (-> command
                                    (select-keys [:course-id :material])
-                                   (assoc :m/id (new-id))))]))
+                                   (assoc ::m/id (new-id))))]))
 
 (defmethod handle-command :delete-course!
   [command course]
   (when course
     [(evt/map->CourseDeleted (-> command
                                  (select-keys [:course-id])
-                                 (assoc :m/id (new-id))))]))
+                                 (assoc ::m/id (new-id))))]))
 
