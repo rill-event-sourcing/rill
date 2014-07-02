d65828f1a4ce8eea9e7fc2c3e0ab1599fbe552d9
Steven Thonus <steven@ln2.nl>
2014-07-02 13:09:37 +0200

    logging in redirect the editor back to where he was
    in a cookie named studyflow_redir_to the full path is set.
    upon succesfull login the user gets redirected

--- login/src/studyflow/login/main.clj
+++ login/src/studyflow/login/main.clj
@@ -111,14 +111,19 @@
   {:status  302
    :headers {"Location" path}})
 
-(defn redirect-path [role]
+(defn default-redirect-path [role]
   (case role
     "editor" publishing-url
     "tester" "https://staging.studyflow.nl"
     "/"))
 
-(defn redirect-home [role]
-  (redirect-to (redirect-path role)))
+(defn redirect-user [cookie-path role]
+  (if cookie-path
+    (redirect-to cookie-path) 
+    (redirect-to (default-redirect-path role))))
+
+(defn get-redirect-cookie [cookies]
+  (:value (cookies "studyflow_redir_to")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Controller
@@ -133,10 +138,10 @@
   (GET "/login" {session :session params :params}
     (layout "login" (login (params :msg) (params :email) (params :password) )))
 
-  (POST "/login" {db :db session :session {:keys [email password]} :params}
+  (POST "/login" {db :db cookies :cookies session :session {:keys [email password]} :params}
     (if-let [user (find-user db email)]
       (if (authenticate user password)
-        (assoc (redirect-home (:role user))
+        (assoc (redirect-user (get-redirect-cookie cookies) (:role user))
                :session (assoc-user session user)
                :cookies {:studyflow_session (:uuid user)}
                )
