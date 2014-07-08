7e78d21cd7fb92f022181446dee787ae391f37ef
Steven Thonus <steven@ln2.nl>
2014-07-08 16:28:27 +0200

    auth server changed to sf.nl for staging

--- publishing/config/environments/production.rb
+++ publishing/config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.auth_server = "http://login-beta.studyflow.nl"
+  config.auth_server = "https://login-beta.studyflow.nl"
   config.redis_server = "login-beta.studyflow.nl"
   config.cookie_domain = "studyflow.nl"
 end
