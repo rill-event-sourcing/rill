e9aec1a12fcd8588e50724958cbaf653b614d326
Remco van 't Veer <remco@remworks.net>
2014-07-09 11:07:57 +0200

    Setup rill dependency properly

--- login/project.clj
+++ login/project.clj
@@ -11,15 +11,18 @@
                  [org.clojure/java.jdbc "0.3.3"]
                  [org.clojure/tools.logging "0.3.0"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
+                 [rill "0.1.0-SNAPSHOT"]
                  [ring/ring-defaults "0.1.0"]
                  [ring-server "0.3.1"]]
-  :plugins [[lein-ring "0.8.10"]
-            [lein-checkouts "1.1.0"]]
+
+  :plugins [[lein-ring "0.8.10"]]
+
   :ring {:init studyflow.login.main/bootstrap!
          :handler studyflow.login.main/app :port 4000}
+
   :aliases {"prepare-database" ["run" "-m" "studyflow.login.prepare-database"]}
 
   :profiles {:dev {:dependencies [[enlive "1.1.5"]
                                   [ring-mock "0.1.5"]]}}
-  
+
   :main studyflow.login.launcher)
