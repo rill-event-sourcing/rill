9b39047ff17f2d69c31d736e88b1855fca75a64c
Joost Diepenmaat <joost@zeekat.nl>
2014-07-11 18:23:08 +0200

    Remove crappy non-test.

--- lib/components/test/studyflow/components_test.clj
+++
@@ -1,7 +0,0 @@
-(ns studyflow.components-test
-  (:require [clojure.test :refer :all]
-            [studyflow.components :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
