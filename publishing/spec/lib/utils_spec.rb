a2f20c8dbb5584f80e9f08ca75a961a2ad954585
Davide Taviani <helios89@gmail.com>
2014-07-16 16:59:24 +0200

    No override for the 'pp' function
    Better naming with "pretty_debug"

--- publishing/spec/lib/utils_spec.rb
+++ publishing/spec/lib/utils_spec.rb
@@ -1,20 +1,20 @@
 require 'rails_helper'
 
-describe "pp" do
+describe "pretty_debug" do
   it "should log the error with a date" do
     text = 'foutmelding'
     expect(Rails.logger).to receive(:debug).exactly(7).times.with(any_args())
-    pp(text)
+    pretty_debug(text)
   end
   it "should log the error to another log" do
     text = 'foutmelding'
     expect(Rails.logger).to receive(:notice).exactly(7).times.with(any_args())
-    pp(text, :notice)
+    pretty_debug(text, :notice)
   end
   it "should log the error with a delta time" do
     text = 'foutmelding'
-    pp(text, :debug, true)
+    pretty_debug(text, :debug, true)
     expect(Rails.logger).to receive(:debug).exactly(3).times.with(any_args())
-    pp(text)
+    pretty_debug(text)
   end
 end
