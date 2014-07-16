3aaa4bc7b18100413f9bef4925baf990617f315d
Davide Taviani <helios89@gmail.com>
2014-07-16 17:02:09 +0200

    Default values for "pre", "post" and "width".
    - pre => ""
    - post => ""
    - width => 10 (em)

--- publishing/db/schema.rb
+++ publishing/db/schema.rb
@@ -56,9 +56,9 @@ ActiveRecord::Schema.define(version: 20140618145746) do
     t.uuid     "question_id"
     t.string   "type"
     t.integer  "position",    limit: 2
-    t.string   "pre"
-    t.string   "post"
-    t.integer  "width",       limit: 2
+    t.string   "pre",                   default: ""
+    t.string   "post",                  default: ""
+    t.integer  "width",       limit: 2, default: 10
     t.datetime "created_at"
     t.datetime "updated_at"
   end
