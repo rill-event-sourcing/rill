f61b18734d4841f84a2d1d060402ad8859e82970
Joost Diepenmaat <joost@zeekat.nl>
2014-07-18 16:09:07 +0200

    WIP

---
+++ super-system/project.clj
@@ -0,0 +1,11 @@
+(defproject studyflow/super-system "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [studyflow/login "0.1.0-SNAPSHOT"]
+                 [studyflow/learning "0.1.0-SNAPSHOT"]
+                 [studyflow/school-administration "0.1.0-SNAPSHOT"]
+                 [com.stuartsierra/component "0.2.1"]])
+
+
+
+
+
