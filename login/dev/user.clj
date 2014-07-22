9ed2d30fbfd3ff15b7c521ade6c2926d0eb7ee94
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-07-22 14:11:23 +0200

    Make login use component system for development

---
+++ login/dev/user.clj
@@ -0,0 +1,42 @@
+(ns user
+  (:require [clojure.tools.logging :as log]
+            [studyflow.login.system :as system]
+            [com.stuartsierra.component :as component]
+            [clojure.tools.namespace.repl :refer [refresh]])
+  (:import [org.apache.log4j Logger]))
+
+
+;; from cascalog playground for swank/slime
+(defn bootstrap-emacs []
+  (let [logger (Logger/getRootLogger)]
+    (doto (. logger (getAppender "stdout"))
+      (.setWriter *out*))
+    (alter-var-root #'clojure.test/*test-out* (constantly *out*))
+    (log/info "Logging to repl")))
+
+(defonce system nil)
+
+(defn init []
+  (alter-var-root #'system (constantly (system/make-system {:jetty-port 4000}))))
+
+(defn start []
+  (bootstrap-emacs)
+  (alter-var-root #'system component/start)
+  :started)
+
+(defn stop []
+  (alter-var-root #'system
+                  (fn [s]
+                    (log/info "stopping system")
+                    (log/info "system is" s)
+                    (when s
+                      (component/stop s)))))
+
+(defn go []
+  (bootstrap-emacs)
+  (init)
+  (start))
+
+(defn reset []
+  (stop)
+  (refresh :after 'user/go))
