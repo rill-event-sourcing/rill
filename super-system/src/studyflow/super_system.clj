23a22db64bd7ae12db27c4f55ba275a7db1e4220
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-07-22 17:42:21 +0200

    Be able to compose systems while sharing some components

--- super-system/src/studyflow/super_system.clj
+++ super-system/src/studyflow/super_system.clj
@@ -2,17 +2,41 @@
   (:require [com.stuartsierra.component :refer [system-map] :as component]
             [studyflow.login.system :as login]
             [studyflow.system :as learning]
+            [studyflow.system.components.memory-event-store :as memory-event-store]
             [clojure.tools.logging :as log]
             [learning-dev-system]))
 
+(defn namespace-system [system prefix exceptions]
+  (let [exceptions (set exceptions)
+        prefix-keyword (fn [kw]
+                         (if (contains? exceptions kw)
+                           kw
+                           (keyword (name prefix) (name kw))))]
+    (zipmap (map prefix-keyword (keys system))
+            (for [c (vals system)]
+              (component/using
+               c
+               (let [deps (component/dependencies c)]
+                 (zipmap (keys deps)
+                         (map prefix-keyword (vals deps)))))))))
+
 (defn make-system [_]
-  (let [learning (learning-dev-system/dev-system (assoc learning-dev-system/dev-config
-                                                   :jetty-port 3000))
-        event-store (:event-store learning)
+  (let [learning (-> (learning-dev-system/dev-system (assoc learning-dev-system/dev-config
+                                                       :jetty-port 3000))
+                     (dissoc :event-store)
+                     (namespace-system :learning [:event-store]))
         login (-> (login/make-system {:jetty-port 4000})
-                  (assoc :event-store event-store))]
-    (system-map :learning-system learning
-                :login-system login)))
+                  (dissoc :event-store)
+                  (namespace-system :login [:event-store]))
+        shared-system {:event-store (component/using
+                                     (memory-event-store/memory-event-store-component)
+                                     [])}]
+    (-> shared-system
+        (into learning)
+        (into login)
+        (->>
+         (mapcat identity)
+         (apply system-map)))))
 
 
 
