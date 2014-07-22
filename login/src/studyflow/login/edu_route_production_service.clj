ccf9f78f9a4a8b31f5634195ac4386aa8923b0cb
Steven Thonus <steven@ln2.nl>
2014-07-22 09:47:21 +0200

    Documentation for API calls to EduRoute added
    The methods used on the EduRoute service are described.

--- login/src/studyflow/login/edu_route_production_service.clj
+++ login/src/studyflow/login/edu_route_production_service.clj
@@ -11,23 +11,30 @@
 
 (deftype EduRouteProductionService []
   EduRouteService
+
+  (check-edu-route-signature [service edu-route-session-id signature]
+    (= (digest/md5 (str edu-route-session-id pre-shared-key)) signature))
+
   ;; the WDSL is downloaded from the "http://www.eduroute.nl/soap/uitgever/uitgeverAPI.php?wsdl"
   ;; the encoded type is renamed to literal
   ;; a Java interface class is extracted using 'wsimport -verbose -Xnocompile -p eduroute wsdl-file'
   ;; This class can now be used to talk to the EduRouteService
 
-  (check-edu-route-signature [service edu-route-session-id signature]
-    (= (digest/md5 (str edu-route-session-id pre-shared-key)) signature))
-
+  ;; Checkt de login van een account via een sessie
+  ;; input: leverancierCode controleCode sessieID
+  ;; output: ResParam ResString (via Holders)
   (get-student-info [service edu-route-session-id]
     (let [soapie (eduroute.EdurouteDistributeurService.)
           myport (.getMyPort soapie)
           h1 (javax.xml.ws.Holder.)
           h2 (javax.xml.ws.Holder.)
-          result (.sessieLogin myport  leveranciers-code controle-code edu-route-session-id h1 h2)]
+          result (.sessieLogin myport leveranciers-code controle-code edu-route-session-id h1 h2)]
       (when (> (.value h1) 0)
         (json/read-str (.value h2)))))
 
+  ;; Vraagt de gegevens van een ASSUnr of Brin op.
+  ;; input: uitgeverCode controleCode assuNr brin
+  ;; output: ResParam ResString (via Holders)
   (get-school-info [service assu-nr brin-code]
     (let [soapie (eduroute.EdurouteDistributeurService.)
           myport (.getMyPort soapie)
@@ -36,6 +43,7 @@
           result (.getSchoolInfo myport leveranciers-code controle-code assu-nr brin-code h1 h2)]
       (when (> (.value h1) 0)
         (json/read-str (.value h2))))))
+
 
 (defn edu-route-production-service
   []
