aaa8668e60589165d259cb03e6e5ec1fd5ad66a9
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-07-22 14:15:02 +0200

    Components for super-system

--- super-system/project.clj
+++ super-system/project.clj
@@ -2,8 +2,25 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [studyflow/login "0.1.0-SNAPSHOT"]
                  [studyflow/learning "0.1.0-SNAPSHOT"]
-                 [studyflow/school-administration "0.1.0-SNAPSHOT"]
-                 [com.stuartsierra/component "0.2.1"]])
+                 #_[studyflow/school-administration "0.1.0-SNAPSHOT"]
+                 [com.stuartsierra/component "0.2.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]]
+  :profiles {:dev
+             {:source-paths ["dev"]
+              :resource-paths ["dev/resources"]
+              :dependencies
+              [[org.clojure/tools.trace "0.7.5"]
+               [org.clojure/tools.namespace "0.2.5"]
+               ;; :dev :dependencies from checkouts not available in
+               ;; this project but will be reloaded by c.t.n.repl/refresh
+               ;; need to find a way to do this programmatically
+               ;; from learning
+               ;; from login
+               [enlive "1.1.5"]
+               [ring-mock "0.1.5"]
+
+]}})
 
 
 
--- super-system/src/studyflow/super_system.clj
+++ super-system/src/studyflow/super_system.clj
@@ -2,10 +2,17 @@
   (:require [com.stuartsierra.component :refer [system-map] :as component]
             [studyflow.login.system :as login]
             [studyflow.system :as learning]
-            [clojure.tools.logging :as log]))
+            [clojure.tools.logging :as log]
+            [learning-dev-system]))
 
 (defn make-system [_]
-  (system-map :login-system (login/make-system {:jetty-port 4000})
-              #_:learning-system #_(learning/prod-system learning/prod-config)))
+  (let [learning (learning-dev-system/dev-system (assoc learning-dev-system/dev-config
+                                                   :jetty-port 3000))
+        event-store (:event-store learning)
+        login (-> (login/make-system {:jetty-port 4000})
+                  (assoc :event-store event-store))]
+    (system-map :learning-system learning
+                :login-system login)))
+
 
 
