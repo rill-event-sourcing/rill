9d6569e4d8857425c531c2bdbd01ce6086b6f5d2
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-07-22 14:12:45 +0200

    Refering to static file breaks super-system

--- learning/test/studyflow/web/api_test.clj
+++ learning/test/studyflow/web/api_test.clj
@@ -1,6 +1,7 @@
 (ns studyflow.web.api-test
   (:use [clojure.test])
   (:require [cheshire.core :as json]
+            [clojure.java.io :as io]
             [clout-link.route :refer [uri-for]]
             [rill.event-store.memory :refer [memory-store]]
             [ring.mock.request :refer [body content-type request]]
@@ -15,7 +16,7 @@
 (deftest test-api-request-handler
   (let [handler (api/make-request-handler (memory-store) (atom {}))
         req (-> (request :put (uri-for routes/update-course-material (:id input)))
-                (body (slurp "test/studyflow/material.json"))
+                (body (slurp (io/resource "dev/material.json")))
                 (content-type "application/json"))
         res (handler req)
         json (json/parse-string (:body res) true)]
