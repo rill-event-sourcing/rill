9fd7144c9ba9c878873b91c689bbcf3c1b704a69
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-07-23 17:16:26 +0200

    Show basic login information in learning

--- learning/src/studyflow/web/browser_resources.clj
+++ learning/src/studyflow/web/browser_resources.clj
@@ -1,13 +1,20 @@
 (ns studyflow.web.browser-resources
   (:require [clout-link.route :as clout]
+            [net.cgrand.enlive-html :as html]
             [ring.middleware.resource :refer [wrap-resource]]
-            [ring.util.response :refer [resource-response]]
             [ring.middleware.file-info :as file-info]
             [studyflow.web.routes :as routes]))
+
+(html/deftemplate course-frame "templates/courses.html"
+  [student]
+  [:span.student-full-name] (html/content (get student :full-name)))
 
 (defn make-request-handler
   []
   (-> (clout/handle routes/get-course-page
-                    (constantly (resource-response "templates/courses.html")))
+                    (fn [req]
+                      {:status 200
+                       :headers {"Content-Type" "text/html"}
+                       :body (apply str (course-frame (get req :student)))}))
       (wrap-resource "/")
       file-info/wrap-file-info))
