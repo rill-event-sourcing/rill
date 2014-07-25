c461d723eea0cb7997ce3641dacfbf7acc861313
Steven Thonus <steven@ln2.nl>
2014-07-25 16:23:21 +0200

    Changed sorting of questions to by name, not text

--- publishing/app/models/question.rb
+++ publishing/app/models/question.rb
@@ -9,7 +9,7 @@ class Question < ActiveRecord::Base
   has_many :line_inputs
   has_many :multiple_choice_inputs
 
-  default_scope { order(:text) }
+  default_scope { order(:name) }
 
   scope :active, -> { where(active: true) }
   scope :for_short_uuid, ->(id) { where(["SUBSTRING(CAST(id AS VARCHAR), 1, 8) = ?", id]) }
