e3879381d22dcde3a152f21eef5871a5a94e240d
Steven Thonus <steven@ln2.nl>
2014-07-25 16:01:53 +0200

    Working code change to set default port to 6000
    Port of development for publishing is now on 6000

--- publishing/config/boot.rb
+++ publishing/config/boot.rb
@@ -2,3 +2,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+# changing default port
+require 'rails/commands/server'
+
+module DefaultOptions
+  def default_options
+    super.merge!(Port: 6000)
+  end
+end
+
+Rails::Server.send(:prepend, DefaultOptions)
