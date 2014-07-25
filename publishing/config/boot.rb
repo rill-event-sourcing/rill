9fddd9bd56122dc18c8f2611f552b10a822145be
Steven Thonus <steven@ln2.nl>
2014-07-25 15:23:59 +0200

    Changed the port of publishing to 6000
    Ports used are now
    learning:        3000, 3001
    login:           4000
    school-admin:    5000
    publishing:      6000

--- publishing/config/boot.rb
+++ publishing/config/boot.rb
@@ -2,3 +2,15 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/commands/server'
+
+module Rails
+  class Server
+    def default_options
+      super.merge({
+        :Port => 6000
+      })
+    end
+  end
+end
