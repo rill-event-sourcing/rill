ce71d693d4e839eda2b0dd9aefc6142746d5b5c1
Gijs Stuurman <gijsstuurmanstudyflow@gmail.com>
2014-07-25 16:10:55 +0200

    Check authorization for students from learning ui

--- learning/src/studyflow/web/api/command.clj
+++ learning/src/studyflow/web/api/command.clj
@@ -4,6 +4,7 @@
             [rill.uuid :refer [uuid]]
             [studyflow.learning.section-test.commands :as section-test-commands]
             [rill.web :refer [wrap-command-handler]]
+            [studyflow.web.authorization :as authorization]
             [studyflow.web.handler-tools :refer [combine-ring-handlers]]
             [studyflow.web.routes :as routes]))
 
@@ -17,29 +18,32 @@ commands."
   (combine-ring-handlers
    (clout/handle
     routes/section-test-init
-    (fn [{{:keys [section-id student-id section-id course-id]} :params :as params}]
-      (section-test-commands/init! (uuid section-id)
-                                   (uuid student-id)
-                                   (uuid course-id))))
+    (authorization/wrap-student-authorization
+     (fn [{{:keys [section-id student-id section-id course-id]} :params :as params}]
+       (section-test-commands/init! (uuid section-id)
+                                    (uuid student-id)
+                                    (uuid course-id)))))
 
    (clout/handle
     routes/section-test-check-answer
-    (fn [{{:keys [section-id student-id course-id question-id]} :params body :body}]
-      (let [{:keys [expected-version inputs]} body]
-        (section-test-commands/check-answer! (uuid section-id)
-                                             (uuid student-id)
-                                             expected-version
-                                             (uuid course-id)
-                                             (uuid question-id)
-                                             inputs))))
+    (authorization/wrap-student-authorization
+     (fn [{{:keys [section-id student-id course-id question-id]} :params body :body}]
+       (let [{:keys [expected-version inputs]} body]
+         (section-test-commands/check-answer! (uuid section-id)
+                                              (uuid student-id)
+                                              expected-version
+                                              (uuid course-id)
+                                              (uuid question-id)
+                                              inputs)))))
    (clout/handle
     routes/section-test-next-question
-    (fn [{{:keys [section-id student-id course-id]} :params
-          {:keys [expected-version] :as body} :body}]
-      (section-test-commands/next-question! (uuid section-id)
-                                            (uuid student-id)
-                                            expected-version
-                                            (uuid course-id))))))
+    (authorization/wrap-student-authorization
+     (fn [{{:keys [section-id student-id course-id]} :params
+           {:keys [expected-version] :as body} :body}]
+       (section-test-commands/next-question! (uuid section-id)
+                                             (uuid student-id)
+                                             expected-version
+                                             (uuid course-id)))))))
 
 (defn make-request-handler
   [event-store]
