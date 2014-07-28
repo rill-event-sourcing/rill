d017883123eb35f96fe787756f8757796ee19820
Davide Taviani <helios89@gmail.com>
2014-07-28 15:55:06 +0200

    Give redis session store uri as command-line parameter

--- learning/src/studyflow/main.clj
+++ learning/src/studyflow/main.clj
@@ -1,14 +1,19 @@
 (ns studyflow.main
-  (:require [clojure.tools.logging :refer [info]]
+  (:require [clojure.tools.logging :as log]
             [studyflow.system :as system]
             [com.stuartsierra.component :as component])
   (:gen-class))
 
-(defn -main [& args]
-  (info "Main Studyflow learning app")
-  (let [s (-> (system/prod-system system/prod-config)
+(defn -main [jetty-port session-store-uri]
+  (log/info "Main Studyflow learning app")
+  (let [s (-> (system/prod-system {:port (Long/parseLong jetty-port)
+                                   :internal-api-port 3001
+                                   :event-store-config {:uri "http://127.0.0.1:2113"
+                                                        :user "admin"
+                                                        :password "changeit"}
+                                   :session-store-config {:uri session-store-uri}})
               component/start)]
     (.addShutdownHook (Runtime/getRuntime)
                       (Thread. (fn []
                                  (component/stop s)
-                                 (info "Stopping is done, bye"))))))
+                                 (log/info "Stopping is done, bye"))))))
