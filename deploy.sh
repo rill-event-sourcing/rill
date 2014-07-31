36676c3576ddc64897c6bc88aca1acde8ee3b642
Steven Thonus <steven@ln2.nl>
2014-07-31 12:29:13 +0200

    changed the deploy script
    usage is just: ./deploy.sh staging b0b6a80746f6182612f946c44b47e054fe819751

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-ssh gitlab-ci "cd gibbon && git pull && ./deploy_local.sh $1 $2"
+# ssh gitlab-ci "cd gibbon && git pull && ./deploy_local.sh $1 $2"
+
+if [ "$#" -ne 2 ]
+then
+    echo "Usage: $0 <environment> <full git sha>"
+    exit 1
+fi
+
+if ! [[ $1 = "staging" || $1 = "production"] ]]
+then
+    echo "Usage: $0 <environment> <full git sha> (with environment: staging OR master!)"
+    exit 1
+fi
+
+if ! [[ $2 =~ ^[a-f0-9]{40}$ ]]
+then
+    echo "Usage: $0 <environment> <FULL git sha> (so FULL sha, nothing else!)"
+    exit 1
+fi
+
+#################################################################
+
+cap $1 deploy -s revision=$2
+
+#################################################################
+echo "all deployed...."
