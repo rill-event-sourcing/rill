d2d83898175f576d66fb5e7a3dc2b46c745d5689
Davide Taviani <helios89@gmail.com>
2014-08-01 16:38:31 +0200

    Better visualization of LaTeX errors

--- publishing/app/helpers/application_helper.rb
+++ publishing/app/helpers/application_helper.rb
@@ -7,6 +7,7 @@ module ApplicationHelper
     matches.each do |array_of_matches|
       match = array_of_matches.first
       rendered_formula = HTTParty.post("http://localhost:16000/", body: "#{match}").parsed_response
+      rendered_formula = content_tag(:div, "\"#{match}\" is not valid LaTeX", class: "alert alert-danger") if rendered_formula == "MathJax error"
       new_text.gsub!("<math>#{match}</math>", rendered_formula)
     end
     new_text
