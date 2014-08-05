2e62a2ba2e9559c829fa9b6a83564fc1e21c0847
Gijs Stuurman <gijs@studyflow.nl>
2014-08-05 10:25:06 +0200

    Remove answers to questions from reading model and UI

--- learning/src/studyflow/learning/read_model/queries.clj
+++ learning/src/studyflow/learning/read_model/queries.clj
@@ -10,10 +10,27 @@
   [m course-id section-id]
   (model/get-section (model/get-course m course-id) section-id))
 
+(defn remove-answers [question]
+  (-> question
+      (update-in [:multiple-choice-input-fields]
+                 (fn [inputs]
+                   (->> (for [input inputs]
+                          (update-in input [:choices]
+                                     (fn [choices]
+                                       (into #{} (map #(dissoc % :correct) choices)))))
+                        (into #{}))))
+      (update-in [:line-input-fields]
+                 (fn [inputs]
+                   (->> (for [input inputs]
+                          (dissoc input :correct-answers))
+                        (into #{}))))
+      (dissoc :worked-out-answer)))
+
 (defn question
   [m course-id section-id question-id]
   (-> m
       (model/get-course course-id)
       (model/get-section section-id)
-      (model/get-question question-id)))
+      (model/get-question question-id)
+      remove-answers))
 
