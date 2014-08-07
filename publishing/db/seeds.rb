1351c71a9000c60d0c1544afaa8ecbf05be51aa7
Davide Taviani <helios89@gmail.com>
2014-08-07 16:24:40 +0200

    Reordering of seeds and adding of Inputs to a Section

--- publishing/db/seeds.rb
+++ publishing/db/seeds.rb
@@ -27,38 +27,35 @@ subtraction = Section.create(title: 'Subtraction', description: 'Too many things
 counting_to_10 = Subsection.create(title: 'Counting to 10', text: '1+1+1+... = 10', section: grouping_by_10)
 summing_up_groups = Subsection.create(title: 'Summing up the number of the groups', text: 'Keep adding!', section: grouping_by_10)
 adding_up_total_and_remainder = Subsection.create(title: 'Adding up everything with the remainder', text: 'Something did not fit in groups...', section: grouping_by_10)
+counting_to_100 = Subsection.create(title: 'Counting to 100', text: '1+1+1+... = 100', section: grouping_by_10)
+summing_up_in_big_groups = Subsection.create(title: 'Summing up in big groups _INPUT_1_', text: 'Keep adding!', section: grouping_by_10)
+adding_up_again = Subsection.create(title: 'Adding up everything again', text: 'Moar additions', section: grouping_by_10)
+grouping_by_10_line_input = LineInput.create(inputable: grouping_by_10, prefix: "666", suffix: "euros")
+grouping_by_10_line_input_answer = Answer.create(value: "123", line_input: grouping_by_10_line_input)
 
 zero_to_the_left = Subsection.create(title: '0 to the left', text: 'It does not matter!', section: position_of_0)
 zero_to_the_right = Subsection.create(title: '0 to the right', text: 'Hey, I multiply by 10!', section: position_of_0)
 zero_to_the_center = Subsection.create(title: '0 in the center', text: 'What now?', section: position_of_0)
-
-adding_small_numbers = Subsection.create(title: 'Adding small numbers', text: 'This looks easy', section: addition)
-adding_medium_numbers = Subsection.create(title: 'Adding medium numbers', text: 'This looks average', section: addition)
-adding_large_numbers = Subsection.create(title: 'Adding large numbers', text: 'This looks hard!', section: addition)
-
-
-subtracting_small_numbers = Subsection.create(title: 'Subtracting small numbers', text: 'This looks easy', section: subtraction)
-subtracting_large_numbers = Subsection.create(title: 'Subtracting medium numbers', text: 'This looks hard enough already', section: subtraction)
-going_negative = Subsection.create(title: 'Going negative', text: 'This looks weird', section: subtraction)
-
-# more subsections to work with
-
-counting_to_100 = Subsection.create(title: 'Counting to 100', text: '1+1+1+... = 100', section: grouping_by_10)
-summing_up_in_big_groups = Subsection.create(title: 'Summing up in big groups', text: 'Keep adding!', section: grouping_by_10)
-adding_up_again = Subsection.create(title: 'Adding up everything again', text: 'Moar additions', section: grouping_by_10)
-
 one_to_the_left = Subsection.create(title: '1 to the left', text: 'It does not matter!', section: position_of_0)
 one_to_the_right = Subsection.create(title: '1 to the right', text: 'Hey, I multiply by 10!', section: position_of_0)
 one_to_the_center = Subsection.create(title: '1 in the center', text: 'What now?', section: position_of_0)
 
+
+adding_small_numbers = Subsection.create(title: 'Adding small numbers', text: 'This looks easy', section: addition)
+adding_medium_numbers = Subsection.create(title: 'Adding medium numbers', text: 'This looks average', section: addition)
+adding_large_numbers = Subsection.create(title: 'Adding large numbers', text: 'This looks hard!', section: addition)
 adding_smallish_numbers = Subsection.create(title: 'Adding smallish numbers', text: 'This looks easy', section: addition)
 adding_mediumish_numbers = Subsection.create(title: 'Adding mediumish numbers', text: 'This looks average', section: addition)
 adding_largish_numbers = Subsection.create(title: 'Adding largish numbers', text: 'This looks hard!', section: addition)
 
 
+subtracting_small_numbers = Subsection.create(title: 'Subtracting small numbers', text: 'This looks easy', section: subtraction)
+subtracting_large_numbers = Subsection.create(title: 'Subtracting medium numbers', text: 'This looks hard enough already', section: subtraction)
+going_negative = Subsection.create(title: 'Going negative', text: 'This looks weird', section: subtraction)
 subtracting_smallish_numbers = Subsection.create(title: 'Subtracting smallish numbers', text: 'This looks easy', section: subtraction)
 subtracting_largish_numbers = Subsection.create(title: 'Subtracting mediumish numbers', text: 'This looks hard enough already', section: subtraction)
 going_negativish = Subsection.create(title: 'Going negativish', text: 'This looks weird', section: subtraction)
+
 
 ## Add some questions and answers for subsections
 
