02b1647f45d83038076e3f36c12af9986ccd61ba
Davide Taviani <helios89@gmail.com>
2014-08-11 14:26:29 +0200

    Moving method used also by (sub)sections

--- publishing/app/controllers/application_controller.rb
+++ publishing/app/controllers/application_controller.rb
@@ -37,4 +37,31 @@ class ApplicationController < ActionController::Base
     end
   end
 
+def set_line_inputs(question, line_inputs_hash)
+    line_inputs_hash.each do |id, values|
+      line_input = question.line_inputs.where(id: id).first
+      line_input.update_attributes(
+        prefix: values[:prefix],
+        suffix: values[:suffix],
+        width: values[:width]
+      )
+      (values[:answers] || {}).each do |id,values|
+        answer = line_input.answers.where(id: id).first
+        answer.update_attributes(values)
+      end
+    end
+  end
+
+  def set_multiple_choice_inputs(question, multiple_choice_inputs_hash)
+    multiple_choice_inputs_hash.each do |id, values|
+      input = question.inputs.where(id: id).first
+      (values[:choices] || {}).each do |id,values|
+        values[:correct] ||= 0
+        choice = input.choices.where(id: id).first
+        choice.update_attributes(values)
+      end
+    end
+  end
+
+
 end
--- publishing/app/controllers/questions_controller.rb
+++ publishing/app/controllers/questions_controller.rb
@@ -56,32 +56,6 @@ class QuestionsController < ApplicationController
 
 private
 
-  def set_line_inputs(question, line_inputs_hash)
-    line_inputs_hash.each do |id, values|
-      line_input = question.line_inputs.where(id: id).first
-      line_input.update_attributes(
-        prefix: values[:prefix],
-        suffix: values[:suffix],
-        width: values[:width]
-      )
-      (values[:answers] || {}).each do |id,values|
-        answer = line_input.answers.where(id: id).first
-        answer.update_attributes(values)
-      end
-    end
-  end
-
-  def set_multiple_choice_inputs(question, multiple_choice_inputs_hash)
-    multiple_choice_inputs_hash.each do |id, values|
-      input = question.inputs.where(id: id).first
-      (values[:choices] || {}).each do |id,values|
-        values[:correct] ||= 0
-        choice = input.choices.where(id: id).first
-        choice.update_attributes(values)
-      end
-    end
-  end
-
   def set_param_objects
     @course = Course.current
     @chapter = @course.chapters.find_by_uuid(params[:chapter_id])
