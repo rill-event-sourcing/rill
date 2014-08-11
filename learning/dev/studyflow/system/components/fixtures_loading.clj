fd4da07483bb5d9bb9dc3ab968f82aa94e0817c5
Gijs Stuurman <gijs@studyflow.nl>
2014-08-11 16:37:44 +0200

    Dev with inline questions in explanation

--- learning/dev/studyflow/system/components/fixtures_loading.clj
+++ learning/dev/studyflow/system/components/fixtures_loading.clj
@@ -11,7 +11,7 @@
   (start [component]
     (info "Starting fixtures-loading-component")
     (let [handler (:handler ring-handler)
-          materials (slurp (io/resource "dev/20140809-staging-material.json"))
+          materials (slurp (io/resource "dev/20140811-dev-material.json"))
           course-id (let [[_ rest] (.split ^String materials ":")
                           [quoted-id] (.split ^String rest ",")
                           [_ id] (.split ^String quoted-id "\"")]
