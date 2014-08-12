8603f6b7d53692a4a95b1aefb268a2c01a5cce71
Steven Thonus <steven@ln2.nl>
2014-08-12 16:04:46 +0200

    added AR-class of Answer to strip whitespace migration

--- publishing/db/migrate/20140811122906_strip_whitespace_from_answers.rb
+++ publishing/db/migrate/20140811122906_strip_whitespace_from_answers.rb
@@ -1,5 +1,8 @@
 class StripWhitespaceFromAnswers < ActiveRecord::Migration
   # This is destructive!
+  class Answer < ActiveRecord::Base
+  end
+
   def up
     Answer.all.each do |answer|
       answer.update_attribute(:value, answer.value.strip)
