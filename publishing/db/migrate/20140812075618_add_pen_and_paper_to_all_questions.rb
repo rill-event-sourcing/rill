e163a0dccba389a46db23134ed6f1795ed06279d
Steven Thonus <steven@ln2.nl>
2014-08-12 12:35:35 +0200

    changed migration due to not running on staging

--- publishing/db/migrate/20140812075618_add_pen_and_paper_to_all_questions.rb
+++ publishing/db/migrate/20140812075618_add_pen_and_paper_to_all_questions.rb
@@ -3,11 +3,8 @@ class AddPenAndPaperToAllQuestions < ActiveRecord::Migration
   class Question < ActiveRecord::Base
   end
 
-
   def up
-    Question.all.each do |question|
-      question.update_attribute(:tools, {"pen_and_paper" => 1})
-    end
+    Question.update_all("tools = '{\"pen_and_paper\" => 1}'")
   end
 
   def down
