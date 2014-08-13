0dceb16cb92bc3e66943e892967831be82ce464c
Steven Thonus <steven@ln2.nl>
2014-08-13 12:25:00 +0200

    fix migration for tools
    tested on staging
    will test again with copy of production database

--- publishing/db/migrate/20140812075618_add_pen_and_paper_to_all_questions.rb
+++ publishing/db/migrate/20140812075618_add_pen_and_paper_to_all_questions.rb
@@ -1,10 +1,13 @@
 class AddPenAndPaperToAllQuestions < ActiveRecord::Migration
   # This migration is destructive
   class Question < ActiveRecord::Base
+    serialize :tools, Hash
   end
 
   def up
-    Question.update_all("tools = '{\"pen_and_paper\" => 1}'")
+    Question.all.each do |question|
+      question.update_attribute(:tools, {"pen_and_paper" => 1})
+    end
   end
 
   def down
