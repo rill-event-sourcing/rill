4920c9ee82cec900fc0c72da6439803e8a277777
Steven Thonus <steven@ln2.nl>
2014-08-15 12:49:35 +0200

    added second school server to staging stack

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,6 @@ set :stage, :staging
 server 'sfstagebalancer.studyflow.nl', port: 1022, user: 'studyflow', roles: %w{balancer}
 server 'sfstagelearning.studyflow.nl', port: 1022, user: 'studyflow', roles: %w{learning},   stack_a: true
 server 'sfstagelogin.studyflow.nl',    port: 1022, user: 'studyflow', roles: %w{login},      stack_a: true
-server 'sfstageschool.studyflow.nl',   port: 1022, user: 'studyflow', roles: %w{school},     stack_a: true
+server 'sfstageschool1.studyflow.nl',  port: 1022, user: 'studyflow', roles: %w{school},     stack_a: true
+server 'sfstageschool2.studyflow.nl',  port: 1022, user: 'studyflow', roles: %w{school},     stack_b: true
 server 'sfstagepublish.studyflow.nl',  port: 1022, user: 'studyflow', roles: %w{publish db}, stack_a: true
