6471fb90b9f723a240495c3cd9cb3c45b08f1c51
Steven Thonus <steven@ln2.nl>
2014-08-15 13:01:36 +0200

    errors when publishing get described better

--- publishing/app/controllers/home_controller.rb
+++ publishing/app/controllers/home_controller.rb
@@ -20,29 +20,30 @@ class HomeController < ApplicationController
     throw Exception.new("Publishing without course selected!") unless course
 
     course_json = JSON.pretty_generate(course.to_publishing_format)
-    url = "#{StudyflowPublishing::Application.config.learning_server}/api/internal/course/#{ course.id }"
+    publishing_url = "#{StudyflowPublishing::Application.config.learning_server}/api/internal/course/#{ course.id }"
 
     if course.errors_when_publishing.any?
       flash[:alert] = "Please fix the errors before publishing"
       redirect_to root_path
     else
-
-
       begin
-        publish_response =  HTTParty.put(url,
+        publish_response =  HTTParty.put(publishing_url,
                                          headers: { 'Content-Type' => 'application/json' },
                                          body: course_json,
                                          timeout: 30)
       rescue Errno::ECONNREFUSED
-        flash[:alert] = "Connection refused"
+        failed_publish_msg = "Connection refused while publishing to: #{ publishing_url }"
       rescue Net::ReadTimeout
-        flash[:alert] = "Timeout"
+        failed_publish_msg = "Timeout while publishing to: #{ publishing_url }"
+      rescue Exception => ex
+        failed_publish_msg = "Unknow exception while publishing to: #{ publishing_url }: #{ ex }"
       end
 
       if publish_response && publish_response.code == 200
         redirect_to root_path, notice: "Course '#{ course }' was succesfully published!"
       else
         flash[:alert] = "Course '#{ course }' was NOT published!"
+        flash[:notice] = failed_publish_msg if failed_publish_msg
         redirect_to root_path
       end
     end
