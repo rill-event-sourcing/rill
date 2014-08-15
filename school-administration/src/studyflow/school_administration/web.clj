d41ee36d2cd43deb5c7575a4dd784c295fc39aa6
Joost Diepenmaat <joost@zeekat.nl>
2014-08-15 12:22:37 +0200

    Put sessions in cookies so we can use multiple servers

--- school-administration/src/studyflow/school_administration/web.clj
+++ school-administration/src/studyflow/school_administration/web.clj
@@ -4,8 +4,10 @@
             [compojure.route :refer [not-found]]
             [studyflow.school-administration.web.command :as command]
             [studyflow.school-administration.web.query :as query]
+            [ring.middleware.session.cookie :refer [cookie-store]]
             [studyflow.school-administration.read-model :as m]
             [ring.middleware.defaults :refer [secure-site-defaults site-defaults wrap-defaults]]
+            
             [ring.util.response :refer [redirect resource-response]]))
 
 (defn wrap-exception-catcher [app]
@@ -27,6 +29,7 @@
 
 (defn studyflow-site-config [config]
   (-> config
+      (assoc-in [:session :store] (cookie-store {:key (byte-array [40 -20 -120 127 67 -33 93 0 -5 59 -113 122 12 -72 -86 99])}))
       (assoc-in [:session :cookie-name] "studyflow_school_session")
       (assoc-in [:security :anti-forgery] false)
       (assoc-in [:static :resources] "school_administration/public")
@@ -45,3 +48,4 @@
       (wrap-defaults (studyflow-site-config (if secure-site-defaults?
                                               secure-site-defaults
                                               site-defaults)))))
+
