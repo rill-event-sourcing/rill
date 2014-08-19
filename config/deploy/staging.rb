73a304303d18aff737ffbf8f2f2f69d58e0afa80
Steven Thonus <steven@ln2.nl>
2014-08-19 12:39:02 +0200

    added extra servers for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,8 +1,10 @@
 set :stage, :staging
 
 server 'sfstagebalancer.studyflow.nl', port: 1022, user: 'studyflow', roles: %w{balancer}
-server 'sfstagelearning.studyflow.nl', port: 1022, user: 'studyflow', roles: %w{learning},   stack_a: true
-server 'sfstagelogin.studyflow.nl',    port: 1022, user: 'studyflow', roles: %w{login},      stack_a: true
+server 'sfstagelearning1.studyflow.nl', port: 1022, user: 'studyflow', roles: %w{learning},   stack_a: true
+# server 'sfstagelearning2.studyflow.nl', port: 1022, user: 'studyflow', roles: %w{learning},   stack_b: true
+server 'sfstagelogin1.studyflow.nl',    port: 1022, user: 'studyflow', roles: %w{login},      stack_a: true
+server 'sfstagelogin2.studyflow.nl',    port: 1022, user: 'studyflow', roles: %w{login},      stack_b: true
 server 'sfstageschool1.studyflow.nl',  port: 1022, user: 'studyflow', roles: %w{school},     stack_a: true
 server 'sfstageschool2.studyflow.nl',  port: 1022, user: 'studyflow', roles: %w{school},     stack_b: true
 server 'sfstagepublish.studyflow.nl',  port: 1022, user: 'studyflow', roles: %w{publish db}, stack_a: true
