e639b3542ca210c4f422846f740c644a48565573
Remco van 't Veer <remco@remworks.net>
2014-08-22 14:15:13 +0200

    Revert claim when main command throws an exception

--- school-administration/src/studyflow/command_tools.clj
+++ school-administration/src/studyflow/command_tools.clj
@@ -1,10 +1,16 @@
 (ns studyflow.command-tools
-  (:require [rill.handler :refer [try-command]]))
+  (:require [clojure.tools.logging :as log]
+            [rill.handler :refer [try-command]]))
 
 (defn with-claim [event-store claim main revert-claim]
   (let [[claim-status :as claim-result] (try-command event-store claim)]
     (if (= :ok claim-status)
-      (let [[main-status :as main-result] (try-command event-store main)] ; TODO catch exception and revert claim
+      (let [[main-status :as main-result]
+            (try (try-command event-store main)
+                 (catch Exception e
+                   (log/error e)
+                   (try-command event-store revert-claim)
+                   (throw e)))]
         (when-not (= :ok main-status)
           (try-command event-store revert-claim))
         main-result)
