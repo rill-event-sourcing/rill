e4e64a7cac06967a260bb0a57dc78fa739aad341
Davide Taviani <helios89@gmail.com>
2014-08-22 16:31:34 +0200

    Compass watching works also with shared sass files

--- login/sass/config.rb
+++ login/sass/config.rb
@@ -8,6 +8,7 @@ sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 
+add_import_path "../../lib/styling"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
