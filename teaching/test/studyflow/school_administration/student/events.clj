12196429fa1faac56b8e7855d8aee4bf38ab1b12
Davide Taviani <helios89@gmail.com>
2014-09-01 14:49:13 +0200

    Copying over the required events and tools to test (similar to login)

---
+++ teaching/test/studyflow/school_administration/student/events.clj
@@ -0,0 +1,47 @@
+(ns studyflow.school-administration.student.events
+  (:require [rill.message :refer [defevent]]
+            [schema.core :as s]))
+
+(def EmailPasswordCredentials
+  {:email s/Str
+   :encryped-password s/Str})
+
+(defevent Created
+  :student-id s/Uuid
+  :full-name s/Str)
+
+(defevent CredentialsAdded
+  :student-id s/Uuid
+  :credentials EmailPasswordCredentials)
+
+(defevent CredentialsChanged
+  :student-id s/Uuid
+  :credentials EmailPasswordCredentials)
+
+(defevent EduRouteCredentialsAdded
+  :student-id s/Uuid
+  :edu-route-id s/Str)
+
+(defevent NameChanged
+  :student-id s/Uuid
+  :full-name s/Str)
+
+(defevent EmailChanged
+  :student-id s/Uuid
+  :email s/Str)
+
+(defevent DepartmentChanged
+  :student-id s/Uuid
+  :department-id s/Uuid)
+
+(defevent ClassAssigned
+  :student-id s/Uuid
+  :department-id s/Uuid
+  :class-name s/Str)
+
+(defevent Imported
+  :student-id s/Uuid
+  :full-name s/Str
+  :department-id s/Uuid
+  :class-name s/Str
+  :credentials EmailPasswordCredentials)
