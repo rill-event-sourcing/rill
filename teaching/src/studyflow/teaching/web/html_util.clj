3053dfed2238889117d73cc716258b426cbd6e96
Davide Taviani <helios89@gmail.com>
2014-09-03 14:55:26 +0200

    Styling fix for long titles in header of teacher completion report

--- teaching/src/studyflow/teaching/web/html_util.clj
+++ teaching/src/studyflow/teaching/web/html_util.clj
@@ -13,12 +13,12 @@
     (include-css "/screen.css")]
    [:body
     [:header
-     [:h1 (h title)]
      (when redirect-urls
        (form/form-to
         {:role "form" :id "logout-form"} [:post (:login redirect-urls)]
         [:input {:type "hidden" :name "_method" :value "DELETE"}]
-        [:button {:type "submit"} "Uitloggen"]))]
+        [:button {:type "submit"} "Uitloggen"]))
+     [:h1 (h title)]]
     [:div.body
      (when warning [:div.warning (h warning)])
      (when message [:div.message (h message)])
