8a9a48df12ba2fa35fb1d92496a02e6e2738106f
Joost Diepenmaat <joost@zeekat.nl>
2014-09-23 12:08:33 +0200

    More reliable logging of SQL exception causes

--- common/src/studyflow/components/uncaught_exception_handler.clj
+++ common/src/studyflow/components/uncaught_exception_handler.clj
@@ -3,14 +3,34 @@
             [com.stuartsierra.component :refer [Lifecycle]]
             [environ.core :refer [env]]
             [ring.util.response :refer [resource-response]])
-  (:import (com.mindscapehq.raygun4java.core RaygunClient)))
+  (:import (com.mindscapehq.raygun4java.core RaygunClient)
+           (java.sql SQLException)
+           (java.lang Error)))
+
+(defn exception-list
+  [throwable]
+  (cond
+   (nil? throwable)
+   nil
+   ;; exceptions that kill a thread get wrapped in java.lang.Error
+   ;; see http://docs.oracle.com/javase/7/docs/api/java/lang/Error.html
+   (and (instance? Error throwable)
+        (.getCause throwable))
+   (recur (.getCause throwable))
+   (instance? SQLException throwable)
+   (lazy-seq (cons throwable (exception-list (.getNextException throwable))))
+   :else
+   [throwable]))
 
 (defn report
   [throwable & [tags meta]]
-  (log/error throwable "Uncaught Exception:" meta)
-  (if-let [raygun-api-key (env :raygun-api-key)]
-    (let [client (RaygunClient. raygun-api-key)]
-      (.Send client throwable tags meta))))
+  (let [exceptions (exception-list throwable)]
+    (doseq [e exceptions]
+      (log/error e "Uncaught Exception:" meta))
+    (if-let [raygun-api-key (env :raygun-api-key)]
+      (let [client (RaygunClient. raygun-api-key)]
+        (doseq [e exceptions]
+          (.Send client e tags meta))))))
 
 (def uncaught-exception-handler
   (reify Thread$UncaughtExceptionHandler
@@ -55,4 +75,8 @@
         .start)
     (clojure.core.async/thread
       (Thread/sleep 2000)
-      (throw (RuntimeException. "TEST EXCEPTION from core.async thread"))))
+      (throw (RuntimeException. "TEST EXCEPTION from core.async thread")))
+    (clojure.core.async/thread
+      (doto (SQLException. "first should have next")
+        (.setNextException (SQLException. "next for first"))
+        (throw))))
