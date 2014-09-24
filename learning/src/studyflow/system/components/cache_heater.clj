32d3e1b1ba95bfef7b916431a2994cad8268180d
Joost Diepenmaat <joost@zeekat.nl>
2014-09-24 15:30:53 +0200

    Preheat the aggregate cache with course material
    This change ensures that any course material that's been published
    will be in the aggregate cache before the web app is made available.
    
    This is important because loading the course material aggregate takes a
    fairly long time and quite a lot of memory. If we don't pre cache this
    it can bring down the application when we get a bunch of commands that
    interact with the course material immediately after startup.

---
+++ learning/src/studyflow/system/components/cache_heater.clj
@@ -0,0 +1,42 @@
+(ns studyflow.system.components.cache-heater
+  (:require [com.stuartsierra.component :refer [Lifecycle]]
+            [studyflow.components.event-channel :refer [channel]]
+            [clojure.tools.logging :refer [info debug spy]]
+            [clojure.core.async :refer [thread <!!]]
+            [studyflow.learning.course.events :as course]
+            [rill.message :as message]
+            [rill.repository :refer [retrieve-aggregate]]
+            [rill.event-channel :as event-channel]
+            [studyflow.web.app-status :refer [healthy-response starting-up-response]]))
+
+(defrecord CacheHeater [event-store event-channel num]
+  Lifecycle
+  (start [component]
+    (info "Starting cache heater")
+    (let [ch (channel event-channel num)
+          done? (atom false)]
+      (thread
+        (loop []
+          (when-let [e (<!! ch)]
+            (when (= (message/type e) ::course/Published)
+              (retrieve-aggregate (:store event-store) (:course-id e)))
+            (when-not (= (message/type e) ::event-channel/CaughtUp)
+              (recur))))
+        (info "Caught up with all events. Cache warmed up")
+        (reset! done? true))
+      (assoc component :done? done?)))
+  (stop [component]
+    component))
+
+(defn cache-heater [num]
+  (map->CacheHeater {:num num}))
+
+(defn wrap-cache-status
+  [handler cache-heater]
+  (fn [{:keys [uri] :as request}]
+    (if @(:done? cache-heater)
+      (if (= uri "/health-check")
+        healthy-response
+        (handler request))
+      starting-up-response)))
+
