79e77719eea98c7f51ca39a262180cb214d29be9
Steven Thonus <steven@ln2.nl>
2014-10-03 12:55:58 +0200

    fresh copy of reporting folder; rebasing failed

---
+++ reporting/project.clj
@@ -0,0 +1,15 @@
+(defproject studyflow/reporting "0.1.0-SNAPSHOT"
+  :description "Reporting"
+  :url "http://studyflow.nl"
+
+  :dependencies [[compojure "1.1.8"]
+                 [hiccup "1.0.5"]
+                 [org.clojure/clojure "1.6.0"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [org.slf4j/slf4j-log4j12 "1.7.5"]
+                 [rill "0.1.0-SNAPSHOT"]
+                 [clojurewerkz/elastisch "2.1.0-beta4"]
+                 [studyflow/components "0.1.0-SNAPSHOT"]
+                 [org.bovinegenius/exploding-fish "0.3.4"]]
+
+  :main studyflow.reporting.main)
