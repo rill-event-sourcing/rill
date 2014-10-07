1f807980c2d9170d1c9e84505bd49d11f420bf12
Joost Diepenmaat <joost@zeekat.nl>
2014-10-07 14:16:30 +0200

    Update studyflow dependencies
    This makes the reporter build again on fresh systems with newer
    studyflow/rill code.

--- reporting/project.clj
+++ reporting/project.clj
@@ -7,9 +7,8 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.3.0"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
-                 [rill "0.1.0-SNAPSHOT"]
                  [clojurewerkz/elastisch "2.1.0-beta4"]
-                 [studyflow/components "0.1.0-SNAPSHOT"]
+                 [studyflow/studyflow "0.1.0-SNAPSHOT"]
                  [org.bovinegenius/exploding-fish "0.3.4"]
                  [clj-time "0.8.0"]]
 
