2a8c98dffeca078666d32c455ac76da498a476b9
Gijs Stuurman <gijs@studyflow.nl>
2014-10-17 12:18:18 +0200

    /listener-check url to see if events are being read, works during and after catch-up in dev and production

--- src/studyflow/web/app_status.clj
+++ src/studyflow/web/app_status.clj
@@ -7,24 +7,35 @@
 
 (defn listen!
   "listen on event-channel"
-  [event-channel status-atom]
+  [event-channel status-atom listener-progress-atom]
   (thread
     (while-let [e (<!! event-channel)]
+               (swap! listener-progress-atom
+                      (fn [old]
+                        {:events-seen (inc (:events-seen old))
+                         :message/timestamp (message/timestamp e)
+                         :message/number (message/number e)
+                         :message/id (message/id e)
+                         :message/type (message/type e)
+                         :seen-at (java.util.Date.)
+                         :cursor (:cursor (meta e))}))
                (when (= (message/type e) :rill.event-channel/CaughtUp)
                  (reset! status-atom :caught-up)))))
 
-(defrecord WebStatusComponent [event-channel num status-atom]
+(defrecord WebStatusComponent [event-channel num status-atom listener-progress-atom]
   Lifecycle
   (start [component]
     (assoc component
-      :event-listener (listen! (channel event-channel num) status-atom)))
+      :event-listener (listen! (channel event-channel num) status-atom listener-progress-atom)))
   (stop [component]
     (when-let [c (channel event-channel num)]
       (close! c))
     (dissoc component :event-listener)))
 
 (defn app-status-component [channel-number]
-  (map->WebStatusComponent {:num channel-number :status-atom (atom nil)}))
+  (map->WebStatusComponent {:num channel-number
+                            :status-atom (atom nil)
+                            :listener-progress-atom (atom {:events-seen 0})}))
 
 (defn caught-up?
   [component]
@@ -40,11 +51,18 @@
    :body "{\"status\":\"up\"}"
    :headers {"Content-Type" "text/plain"}})
 
+(defn listener-check [request app-status-component]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (str (pr-str @(:listener-progress-atom app-status-component)) "\r\n")})
+
 (defn wrap-app-status
   [handler app-status-component]
   (fn [{:keys [uri] :as request}]
-    (if (caught-up? app-status-component)
-      (if (= uri "/health-check")
-        healthy-response
-        (handler request))
-      starting-up-response)))
+    (if (= uri "/listener-check")
+      (listener-check request app-status-component)
+      (if (caught-up? app-status-component)
+        (if (= uri "/health-check")
+          healthy-response
+          (handler request))
+        starting-up-response))))
