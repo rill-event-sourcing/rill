cde53e5e79afa36cb82f43309193b21e1687bbb9
Davide Taviani <helios89@gmail.com>
2014-11-13 11:13:47 +0100

    Tests for moveup movedown

--- publishing/spec/models/choice_spec.rb
+++ publishing/spec/models/choice_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe Choice, type: :model do
   it {is_expected.to belong_to :multiple_choice_input}
 
   before do
-    @choice = create(:choice)
-    @choice2 = create(:choice)
+    @mc = create(:multiple_choice_input)
+    @choice = create(:choice, position: 1, multiple_choice_input: @mc)
+    @choice2 = create(:choice, position: 2, multiple_choice_input: @mc)
   end
 
   it "should return an abbreviated uuid" do
@@ -21,6 +22,17 @@ RSpec.describe Choice, type: :model do
   it "should not throw an ActiveRecord::RecordNotFound when not found by an abbreviated uuid with 'with_404' = false" do
     expect{Choice.find_by_uuid('1a31a31a', false)}.not_to raise_error
     expect(Choice.find_by_uuid('1a31a31a', false)).to eq nil
+  end
+
+  it "should correctly reorder choices" do
+    @choice.move_lower
+    expect(@choice.position).to eq 2
+    @choice2.reload
+    expect(@choice2.position).to eq 1
+    @choice.move_higher
+    expect(@choice.position).to eq 1
+    @choice2.reload
+    expect(@choice2.position).to eq 2
   end
 
   it "should throw an StudyflowPublishing::ShortUuidDoubleError when found multiple choices by an abbreviated uuid" do
