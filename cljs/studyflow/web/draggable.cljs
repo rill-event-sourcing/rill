9cdf5095e0b311b35e7b65d19c1f24f0af5b172e
Gijs Stuurman <gijs@studyflow.nl>
2014-11-21 12:06:42 +0100

    Calculator on ipad undraggable, remember last position otherwise

--- cljs/studyflow/web/draggable.cljs
+++ cljs/studyflow/web/draggable.cljs
@@ -2,7 +2,12 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [goog.events :as events]
-            [goog.fx.Dragger :as fxdrag]))
+            [goog.fx.Dragger :as fxdrag]
+            [studyflow.web.ipad :refer [ipad?]]))
+
+;; will keep the location between switching calculator on/off
+(def position (atom {:top 90
+                     :left 90}))
 
 (defn draggable-item
   [view position-cursor]
@@ -10,18 +15,38 @@
     (reify
       om/IRender
       (render [_]
-        (dom/div #js {:style #js {:position "fixed"
-                                  :zIndex 10000}}
+        (dom/div #js {:style
+                      #js {:position "fixed"
+                           :zIndex 10000
+                           :top (if ipad?
+                                  10
+                                  (:top @position))
+                           :left (if ipad?
+                                   00
+                                   (:left @position))}}
+                 (when (and (not ipad?)
+                            (om/get-state owner :dragging))
+                   (dom/div #js {:style #js {:position "absolute"
+                                             :top 40
+                                             :width 322
+                                             :height 460
+                                             :zIndex 9999}}))
                  (om/build view item)))
       om/IDidMount
       (did-mount [_]
-        (let [el (om/get-node owner)
-              dragger (goog.fx.Dragger. el)]
-          (events/listen dragger
-                         fxdrag/EventType.START
-                         (fn [event]
-                           (om/update! item :dragging true)))
-          (events/listen dragger
-                         fxdrag/EventType.END
-                         (fn [event]
-                           (om/update! item :dragging false))))))))
+        (when-not ipad?
+          (let [el (om/get-node owner)
+                dragger (goog.fx.Dragger. el)]
+            (events/listen dragger
+                           fxdrag/EventType.START
+                           (fn [event]
+                             (om/set-state! owner :dragging true)))
+            (events/listen dragger
+                           fxdrag/EventType.DRAG
+                           (fn [event]
+                             (reset! position {:top (. event -top)
+                                               :left (. event -left)})))
+            (events/listen dragger
+                           fxdrag/EventType.END
+                           (fn [event]
+                             (om/set-state! owner :dragging false)))))))))
