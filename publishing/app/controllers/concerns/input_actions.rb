b37bec1d37ee5a21d4debea508ddc1da5507f43a
Steven Thonus <steven@ln2.nl>
2014-11-26 09:12:00 +0100

    check for nil values to prevent errors on save

--- publishing/app/controllers/concerns/input_actions.rb
+++ publishing/app/controllers/concerns/input_actions.rb
@@ -9,15 +9,17 @@ module InputActions
   def set_line_inputs(question, line_inputs_hash)
     line_inputs_hash.each do |id, values|
       line_input = question.line_inputs.where(id: id).first
-      line_input.update_attributes(
-        style: values[:style],
-        width: values[:width],
-        prefix: values[:prefix],
-        suffix: values[:suffix]
-      )
-      (values[:answers] || {}).each do |id,values|
-        answer = line_input.answers.where(id: id).first
-        answer.update_attributes(values)
+      if line_input
+        line_input.update_attributes(
+          style: values[:style],
+          width: values[:width],
+          prefix: values[:prefix],
+          suffix: values[:suffix]
+        )
+        (values[:answers] || {}).each do |id, values|
+          answer = line_input.answers.where(id: id).first
+          answer.update_attributes(values) if answer
+        end
       end
     end
   end
@@ -25,10 +27,12 @@ module InputActions
   def set_multiple_choice_inputs(question, multiple_choice_inputs_hash)
     multiple_choice_inputs_hash.each do |id, values|
       input = question.inputs.where(id: id).first
-      (values[:choices] || {}).each do |id,values|
-        values[:correct] ||= 0
-        choice = input.choices.where(id: id).first
-        choice.update_attributes(values)
+      if input
+        (values[:choices] || {}).each do |id,values|
+          values[:correct] ||= 0
+          choice = input.choices.where(id: id).first
+          choice.update_attributes(values) if choice
+        end
       end
     end
   end
--- publishing/app/controllers/concerns/reflection_actions.rb
+++ publishing/app/controllers/concerns/reflection_actions.rb
@@ -9,8 +9,10 @@ module ReflectionActions
   def set_reflections(section, reflections_hash)
     reflections_hash.each do |id, values|
       reflection = section.reflections.where(id: id).first
-      reflection.update_attributes(content: values[:content],
-                                   answer: values[:answer])
+      if reflection
+        reflection.update_attributes(content: values[:content],
+                                     answer: values[:answer])
+      end
     end
   end
 end
