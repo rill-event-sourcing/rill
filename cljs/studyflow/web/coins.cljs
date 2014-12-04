e92953ab11e0d5da06262653acdd7636d1ad3f79
Edo van Royen <edo@edovanroyen.com>
2014-12-04 15:08:04 +0100

    Gaming styling

--- cljs/studyflow/web/coins.cljs
+++ cljs/studyflow/web/coins.cljs
@@ -9,7 +9,7 @@
     (render [_]
       (let [student-id (get-in cursor [:static :student :id])
             course-id (get-in cursor [:static :course-id])]
-        (dom/div #js {:id "coins_heading"}
+        (dom/div #js {:className "m-gaming"}
                  (dom/button #js {:onClick (fn []
                                              (let [open? (om/get-state owner :open?)]
                                                (om/set-state! owner :open? (not open?))
@@ -18,11 +18,12 @@
                                                      (async/put! ["data/leaderboard" course-id student-id])))))
                                   :className "btn yellow"}
                              (get-in cursor [:view :course-material :total-coins])
-                             " $")
+                             )
                  (when (om/get-state owner :open?)
                    (if-let [data (get-in cursor [:view :leaderboard :data])]
-                     (dom/table #js {:id "leaderboard"}
-                                (dom/caption nil "Meeste $ de afgelopen 7 dagen")
+                     (dom/div #js {:className "leaderboard-dropdown"}
+                     (dom/h2 #js {:className "leaderboard-heading"} "Meeste" (dom/span #js {:className "inline-coin"}) " de afgelopen 7 dagen")
+                                (dom/table #js {:className "leaderboard-table"}
                                 (apply dom/tbody nil
                                        (map (fn [[pos id coins full-name]]
                                               (dom/tr #js {:className (str "leaderboard-row" (when (= id student-id)
@@ -30,6 +31,6 @@
 
                                                       (dom/td #js {:className "position"} pos ".")
                                                       (dom/td nil full-name)
-                                                      (dom/td #js {:className "coins"} coins " $")))
-                                            data)))
+                                                      (dom/td #js {:className "coins"} coins)))
+                                            data))))
                      "laden....")))))))
