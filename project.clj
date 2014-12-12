9b83d36583fe78d008d74e11a11d71198c86bda7
Joost Diepenmaat <joost@zeekat.nl>
2014-12-12 19:33:42 +0100

    Release and git cleaning

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
 (defproject rill/rill "0.1.6"
+  :description "An Event Sourcing Toolkit"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/rill-event-sourcing/rill"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [org.clojure/tools.logging "0.2.6"]
