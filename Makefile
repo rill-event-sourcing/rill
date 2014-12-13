d01e82b5c34c0b080b531f9f72b49a59fc51aed6
Joost Diepenmaat <joost@zeekat.nl>
2014-12-13 10:09:37 +0100

    Split out durable event store implementations

--- Makefile
+++ Makefile
@@ -3,15 +3,16 @@ default: test
 .PHONY: test default install
 
 test:
-	dropdb rill_test 2> /dev/null || true
-	createdb rill_test
-	psql rill_test < psql_schema.sql
-	psql rill_test -c "GRANT ALL ON rill_events TO studyflow; GRANT ALL ON SEQUENCE rill_events_insert_order_seq TO studyflow;"
-	PSQL_EVENT_STORE_URI="postgresql://localhost:5432/rill_test?user=studyflow&password=studyflow" lein test
-	dropdb rill_test || true
+	lein test
+	lein install
+	make -C rill-psql test
 
 install:
 	lein install
 
 clean:
 	lein clean
+
+jar:
+	lein jar
+	cd rill-psql && lein jar
--- rill-psql/project.clj
+++ rill-psql/project.clj
@@ -1,15 +1,11 @@
-(defproject rill/rill "0.1.7-SNAPSHOT"
+(defproject rill/rill-psql "0.1.7-SNAPSHOT"
   :description "An Event Sourcing Toolkit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/rill-event-sourcing/rill"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [prismatic/schema "0.2.2"]
-                 [slingshot "0.10.3"]
-                 [environ "0.5.0"]
-                 [identifiers "1.1.0"]
+                 [rill/rill "0.1.7-SNAPSHOT"]
                  [org.clojure/java.jdbc "0.3.4"]
                  [postgresql "9.1-901.jdbc4"]
                  [com.taoensso/nippy "2.6.3"]
