c485be761ef06f10f6e1550c4cac7e1c1633218a
Joost Diepenmaat <joost@zeekat.nl>
2015-03-18 14:11:15 +0100

    Document changes

--- rill-psql/project.clj
+++ rill-psql/project.clj
@@ -1,11 +1,11 @@
-(defproject rill/rill-psql "0.2.0-RC1"
+(defproject rill/rill-psql "0.2.0-RC2"
   :description "An Event Sourcing Toolkit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/rill-event-sourcing/rill"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [rill/rill "0.2.0-RC1"]
+                 [rill/rill "0.2.0-RC2"]
                  [org.clojure/java.jdbc "0.3.4"]
                  [postgresql "9.1-901.jdbc4"]
                  [com.taoensso/nippy "2.6.3"]
