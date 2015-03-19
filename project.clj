d23ea3353cd8ccc8b4f7eff67cd8e332a3ecf325
Joost Diepenmaat <joost@zeekat.nl>
2015-03-19 10:04:51 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rill/rill "0.2.0-RC2"
+(defproject rill/rill "0.2.0-RC3"
   :description "An Event Sourcing Toolkit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
