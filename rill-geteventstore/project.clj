b21d6d930bd6dab65d22fea2123066a9dc522c46
Joost Diepenmaat <joost@zeekat.nl>
2015-12-16 20:24:23 +0100

    Remove old and broken geteventstore implementation

--- rill-geteventstore/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject rill/rill-geteventstore "0.1.7-SNAPSHOT"
-  :description "An Event Sourcing Toolkit"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :url "https://github.com/rill-event-sourcing/rill"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [rill/rill "0.1.7-SNAPSHOT"]
-                 [cheshire "5.3.1"]
-                 [clj-http "0.9.1"]])
