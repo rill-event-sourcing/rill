40c11abde68cdd21a5948359140814d7e1f3f20c
Joost Diepenmaat <joost@zeekat.nl>
2015-12-17 14:33:59 +0100

    Prepare 0.2.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rill/rill "0.2.0-RC4"
+(defproject rill-event-sourcing/rill "0.2.0"
   :description "An Event Sourcing Toolkit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -13,9 +13,10 @@
                  [org.clojure/java.jdbc "0.3.4"]
                  [postgresql "9.1-901.jdbc4"]
                  [com.taoensso/nippy "2.6.3"]]
-  :profiles {:dev {:plugins [[lein-repack "0.2.8"]]}}
+  :profiles {:dev {:plugins [[lein-repack "0.2.8"]
+                             [lein-set-version "0.4.1"]]}}
+  :set-version
+  {:updates [{:path "README.org" :no-snapshot true}]}
   :repack [{:path "src"
             :type :clojure
             :levels 2}])
-
-
