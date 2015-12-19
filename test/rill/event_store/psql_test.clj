0949543a6fc6c3a99362dcf079ec1fb4b9c6ef0d
Joost Diepenmaat <joost@zeekat.nl>
2015-12-19 16:35:03 +0100

    Fix accidental rename of environment variable

--- test/rill/event_store/psql_test.clj
+++ test/rill/event_store/psql_test.clj
@@ -8,17 +8,17 @@
              :password (System/getenv "RILL_POSTGRES_PASSWORD")
              :hostname (System/getenv "RILL_POSTGRES_HOST")
              :port (System/getenv "RILL_POSTGRES_PORT")
-             :database (System/getenv "RILL_POSTGRES_DATABASE")})
+             :database (System/getenv "RILL_POSTGRES_DB")})
 
 (defn get-clean-psql-store! [config]
   (tools/clear-db! config)
   (psql-event-store (tools/connection config)))
 
 (deftest test-psql-store
-  (if (System/getenv "RILL_POSTGRES_DATABASE")
+  (if (System/getenv "RILL_POSTGRES_DB")
     (do (tools/load-schema! config)
         (test-store #(get-clean-psql-store! config)))
-    (println "Skipped tests for rill postgres backend. Set at least RILL_POSTGRES_DATABASE environment variable to configure and enable tests")))
+    (println "Skipped tests for rill postgres backend. Set at least RILL_POSTGRES_DB environment variable to configure and enable tests")))
 
 
 
